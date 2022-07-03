843a959d522f69eb97e15e7a8504fc5aa22aadc4
steven-yn <steven-yn>
2022-07-03 12:27:05 +0900

    test getInitialProps

--- petbook_fe/pages/findfriend.tsx
+++ petbook_fe/pages/findfriend.tsx
@@ -7,6 +7,7 @@ import FriendProfileCard from "../components/common/CardUI/ProfileCard";
 import TopNav from "../components/TopNav";
 import styled from "styled-components";
 import FriendCardContainer from "../containers/card/FriendCardContainer";
+import localConsole from "../lib/localConsole";
 
 const FriendProfileList = styled(Box)`
   display: flex;
@@ -16,7 +17,9 @@ const FriendProfileList = styled(Box)`
   height: 15rem;
 `;
 
-const FindHome: NextPage = () => {
+const FindHome: NextPage = ({ tests }: any) => {
+  localConsole.log(tests);
+
   return (
     <>
       <HtmlHeader />
@@ -26,8 +29,31 @@ const FindHome: NextPage = () => {
       {/* 본문영역 */}
       <FriendCardContainer />
       <MapComponent />
+      test data : {tests}
     </>
   );
 };
+
+FindHome.getInitialProps = async (ctx) => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  const json = await res.json();
+
+  return { tests: json.title };
+};
+
+// 1. 서버 사이드에서 가져올 리소스 정의하기
+
+// const data1Resource = createResource({
+//   key: 'data-1',
+//   fetcher: async () => {
+//     return fetch('https://jsonplaceholder.typicode.com/posts/1')
+//   }
+// })
+
+// function Page() {
+//   return <ComponentA />
+// }
+
+// Page.requiredResources = [data1Resource];
 
 export default FindHome;
