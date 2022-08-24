7613eee7dc798b02d9de49302925f3e2737aa7ac
Eunbi Kang <jenabill@naver.com>
2022-08-24 20:31:24 +0900

    add: 커뮤니티 메인페이지에 필요한 MainSection 컴포넌트

---
+++ petbook_fe/components/community/MainSection.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+
+interface MainSectionProps {
+  title: string;
+  description: string;
+  children: React.ReactNode;
+}
+
+const MainSection = ({ title, description, children }: MainSectionProps) => {
+  return (
+    <Section>
+      <Heading>{title}</Heading>
+      <Text>{description}</Text>
+      {children}
+    </Section>
+  );
+};
+
+export default MainSection;
+
+const Section = styled.section`
+  padding-bottom: 150px;
+`;
+
+const Heading = styled.h2`
+  color: #ff2e00;
+  font-family: "Montserrat", sans-serif;
+  font-size: 24px;
+  font-weight: 700;
+  margin-bottom: 10px;
+`;
+
+const Text = styled.p`
+  font-weight: 700;
+  font-size: 30px;
+  margin-bottom: 32px;
+`;
