1bc199b1641eda4194729fbf5ede686ccd7dc4ab
Eunbi Kang <jenabill@naver.com>
2022-08-25 13:45:32 +0900

    fix: useSlide scrollTo 로직 수정

--- petbook_fe/components/community/popular/useSlide.ts
+++ petbook_fe/components/community/popular/useSlide.ts
@@ -3,21 +3,22 @@ import { MutableRefObject } from "react";
 export const useSlide = (root: MutableRefObject<Element | null>) => {
   const onRightSlide = () => {
     if (!root.current) return;
-    const remain = root.current.scrollWidth - root.current.scrollLeft;
+    const position = root.current.scrollLeft;
+    const remain = root.current.scrollWidth - position;
     const width = root.current.clientWidth;
     root.current.scrollTo({
       top: 0,
-      left: remain > width ? width : remain,
+      left: remain > width ? position + width : position + remain,
       behavior: "smooth",
     });
   };
   const onLeftSlide = () => {
     if (!root.current) return;
-    const remain = root.current.scrollLeft;
+    const position = root.current.scrollLeft;
     const width = root.current.clientWidth;
     root.current.scrollTo({
       top: 0,
-      left: remain > width ? -width : -remain,
+      left: position > width ? position - width : 0,
       behavior: "smooth",
     });
   };
