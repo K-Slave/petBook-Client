dd9c3b73802ea68c108889b5a7087e054bacafd0
Eunbi Kang <jenabill@naver.com>
2022-08-25 19:24:37 +0900

    refactor: PostList 컴포넌트 (추후 타입 변경 예정)

--- petbook_fe/components/community/post/PostList.tsx
+++ petbook_fe/components/community/post/PostList.tsx
@@ -1,96 +1,17 @@
 import styled from "styled-components";
 import PostItem from "./PostItem";
 
-const dummy = [
-  {
-    avatar: "",
-    username: "arin",
-    date: "2022.08.24",
-    title:
-      "질문이 들어갑니다 들어갑니다 들어갑니다 들어갑니다 들어갑니다 들어갑니다 들어갑니다",
-    content: `내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다`,
-    tagList: ["태그", "태그", "태그"],
-    likeCnt: 1,
-    commentCnt: 1,
-    viewCnt: 1,
-    previewImage: "",
-  },
-  {
-    avatar: "",
-    username: "arin",
-    date: "2022.08.24",
-    title:
-      "질문이 들어갑니다 들어갑니다 들어갑니다 들어갑니다 들어갑니다 들어갑니다 들어갑니다",
-    content: `내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다`,
-    tagList: ["태그", "태그", "태그"],
-    likeCnt: 1,
-    commentCnt: 1,
-    viewCnt: 1,
-    previewImage: "image",
-  },
-  {
-    avatar: "",
-    username: "arin",
-    date: "2022.08.24",
-    title:
-      "질문이 들어갑니다 들어갑니다 들어갑니다 들어갑니다 들어갑니다 들어갑니다 들어갑니다",
-    content: `내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다`,
-    tagList: ["태그", "태그", "태그"],
-    likeCnt: 1,
-    commentCnt: 1,
-    viewCnt: 1,
-    previewImage: "",
-  },
-  {
-    avatar: "",
-    username: "arin",
-    date: "2022.08.24",
-    title:
-      "질문이 들어갑니다 들어갑니다 들어갑니다 들어갑니다 들어갑니다 들어갑니다 들어갑니다",
-    content: `내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다`,
-    tagList: ["태그", "태그", "태그"],
-    likeCnt: 1,
-    commentCnt: 1,
-    viewCnt: 1,
-    previewImage: "image",
-  },
-  {
-    avatar: "",
-    username: "arin",
-    date: "2022.08.24",
-    title:
-      "질문이 들어갑니다 들어갑니다 들어갑니다 들어갑니다 들어갑니다 들어갑니다 들어갑니다",
-    content: `내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다`,
-    tagList: ["태그", "태그", "태그"],
-    likeCnt: 1,
-    commentCnt: 1,
-    viewCnt: 1,
-    previewImage: "",
-  },
-  {
-    avatar: "",
-    username: "arin",
-    date: "2022.08.24",
-    title:
-      "질문이 들어갑니다 들어갑니다 들어갑니다 들어갑니다 들어갑니다 들어갑니다 들어갑니다",
-    content: `내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다`,
-    tagList: ["태그", "태그", "태그"],
-    likeCnt: 1,
-    commentCnt: 1,
-    viewCnt: 1,
-    previewImage: "image",
-  },
-];
+interface PostListProps {
+  posts: any;
+}
 
-const PostList = () => {
+const PostList = ({ posts }: PostListProps) => {
   return (
-    <>
-      <List>
-        {dummy.map((item, index) => (
-          <PostItem {...item} key={index} />
-        ))}
-      </List>
-    </>
+    <List>
+      {posts.map((item: any, index: number) => (
+        <PostItem {...item} key={index} />
+      ))}
+    </List>
   );
 };
 
