12477046ea1d1cff5e8d6ccb76b936421ee6ac0d
steven-yn <steven-yn>
2022-08-27 15:51:59 +0900

    add quill

---
+++ petbook_fe/pages/community/index.tsx
@@ -0,0 +1,38 @@
+import { NextPage } from "next";
+import styled from "styled-components";
+import HtmlHeader from "../../components/common/HtmlHeader";
+// import About from "../components/community/about/About";
+// import PopularPostSection from "../components/community/popular/PopularPostSection";
+// import PostSection from "../components/community/post/PostSection";
+// import VoteSection from "../components/community/vote/VoteSection";
+// import WriteButton from "../components/community/WriteButton";
+
+const Community: NextPage = () => {
+  return (
+    <>
+      <HtmlHeader />
+      <Container>
+        {/* <About /> */}
+        <Sections>
+          {/* <VoteSection />
+          <PopularPostSection />
+          <PostSection /> */}
+        </Sections>
+        {/* <WriteButton /> */}
+      </Container>
+    </>
+  );
+};
+
+export default Community;
+
+const Container = styled.div`
+  background-color: #fffbf4;
+`;
+
+const Sections = styled.div`
+  width: 90vw;
+  max-width: 1280px;
+  margin: 0 auto;
+  padding-top: 80px;
+`;
