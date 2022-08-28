6dcaa3ed8cb98180a6de1a9d9eeb2f4a3ff4ff95
jj2084jj <whdms3368@gmail.com>
2022-08-28 11:31:58 +0900

    fixed-register-ui

--- petbook_fe/pages/login/register.tsx
+++ petbook_fe/pages/login/register.tsx
@@ -1,6 +1,6 @@
 import HtmlHeader from "../../components/common/HtmlHeader";
 import TopNav from "../../components/TopNav";
-import ValidationInput from "../../components/common/validationInput";
+import ValidationInput from "../../components/common/ValidationInput";
 
 //
 import styled from "styled-components";
@@ -9,10 +9,49 @@ const Main = styled.main`
   height: calc(100vh - 61px);
   overflow: auto;
   background-color: var(--bg);
+
   .formWrap {
     max-width: 679px;
     width: 100%;
     margin: 0 auto;
+    margin-top: 76px;
+    > div {
+      margin-bottom: 26px;
+      &:last-child {
+        margin-bottom: 0;
+      }
+      &.flex {
+        > div {
+          width: 50%;
+          &:first-child {
+            margin-right: 9px;
+          }
+        }
+      }
+    }
+    .box {
+      position: relative;
+      label {
+        position: absolute;
+        top: 50%;
+        left: 30px;
+        transform: translateY(-50%);
+        color: #000000;
+        opacity: 68%;
+      }
+      input {
+        padding-left: 140px;
+        line-height: 30px;
+        &::placeholder {
+          color: #b7b7b7;
+        }
+      }
+    }
+    .err_box {
+      color: #ff6e4e;
+      line-height: 30px;
+      margin-top: 4px;
+    }
   }
 `;
 
