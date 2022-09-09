7d9050d743fcbf96b7fdd9c61dd14fcf461118b8
Eunbi Kang <jenabill@naver.com>
2022-09-09 20:32:18 +0900

    refactor: ImageSlider component

--- petbook_fe/components/community/detail/ImageSlider.tsx
+++ petbook_fe/components/community/detail/ImageSlider.tsx
@@ -1,14 +1,27 @@
+import { useSetRecoilState } from "recoil";
 import styled from "styled-components";
+import { sliderModalState } from "../../../atoms/componentAtoms/community/sliderModal";
 
 interface ImageSliderProps {
   images: string[];
 }
 
 const ImageSlider = ({ images }: ImageSliderProps) => {
+  const setModalState = useSetRecoilState(sliderModalState);
   return images.length !== 0 ? (
     <Slider>
       {images.map((image, index) => (
-        <DummyImage key={index} />
+        <DummyImage
+          src={image}
+          key={index}
+          onClick={() =>
+            setModalState({
+              show: true,
+              currentIndex: index,
+              prevIndex: index === 0 ? images.length - 1 : index - 1,
+            })
+          }
+        />
       ))}
     </Slider>
   ) : (
@@ -29,12 +42,14 @@ const Slider = styled.div`
   }
 `;
 
-const DummyImage = styled.div`
+const DummyImage = styled.img`
   width: 354px;
   height: 354px;
   background-color: #d9d9d9;
   border-radius: 16px;
   flex-shrink: 0;
+  object-fit: cover;
+  cursor: pointer;
 `;
 
 const Spacer = styled.div`
