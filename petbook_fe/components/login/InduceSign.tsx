cbc993b6e057cab102077bc821b3be3f8ef2ad55
Mac <mac@Macs-MacBook-Pro.local>
2022-09-12 16:32:47 +0900

    route-login-set

--- petbook_fe/components/login/InduceSign.tsx
+++ petbook_fe/components/login/InduceSign.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useRouter } from "next/router";
 
 const Container = styled.div`
   padding: 41px 35px;
@@ -7,6 +8,7 @@ const Container = styled.div`
   color: white;
   background-color: var(--main);
   margin-top: 32px;
+  cursor: pointer;
   p {
     font-size: 16px;
     font-weight: 400;
@@ -20,9 +22,10 @@ const Container = styled.div`
 `;
 
 const InduceSign = () => {
+  const router = useRouter();
   return (
     <>
-      <Container>
+      <Container onClick={() => router.push("/login/register")}>
         <p>아직 Petbook 계정이 없으신가요?</p>
         <h3>
           회원가입을 통해 Petbook 유저들과 <br /> 소통할 수 있어요!
--- petbook_fe/pages/login/register.tsx
+++ petbook_fe/pages/login/register.tsx
@@ -5,6 +5,10 @@ import ValidationInput from "../../components/common/ValidationInput";
 //
 import styled from "styled-components";
 
+//
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
 const Main = styled.main`
   height: calc(100vh - 61px);
   overflow: auto;
@@ -54,47 +58,93 @@ const Main = styled.main`
       line-height: 30px;
       margin-top: 4px;
     }
+
+    &.small {
+      max-width: 520px;
+      text-align: center;
+      p {
+        font-size: 20px;
+        margin-bottom: 14px;
+      }
+      h3 {
+        margin-bottom: 55px;
+        font-size: 28px;
+      }
+      .submit_sucess_img {
+        height: 297px;
+        width: 100%;
+        margin-bottom: 28px;
+        background-color: var(--test);
+      }
+    }
   }
 `;
 
 const SubmitBtn = styled.div`
   width: 100%;
   margin: 60px 0 0;
-  a {
-    display: block;
-    padding: 20px 0;
-    text-align: center;
-    font-weight: 700;
-    font-size: 20px;
-    color: white;
-    border-radius: 12px;
-    box-sizing: border-box;
-    background-color: var(--main);
-  }
+  display: block;
+  padding: 20px 0;
+  text-align: center;
+  font-weight: 700;
+  font-size: 20px;
+  color: white;
+  border-radius: 12px;
+  box-sizing: border-box;
+  background-color: var(--main);
+  cursor: pointer;
 `;
 
 const Register = () => {
+  const router = useRouter();
+  const [signActive, setSignActive] = useState(false);
+
+  useEffect(() => {
+    if (router.query.state === "true") {
+      setSignActive(true);
+    }
+  }, [router]);
+
+  const Sign = () => {
+    //여기서 서버 호출
+    router.replace({
+      pathname: "/login/register",
+      query: { state: true },
+    });
+  };
+
   return (
     <>
       <HtmlHeader />
       <TopNav />
       <Main>
-        <div className="formWrap">
-          <ValidationInput current={"이메일"} />
-          <ValidationInput current={"인증번호"} />
-          <div className="flex">
+        {signActive ? (
+          <div className="formWrap small">
+            <p>회원가입 완료!</p>
+            <h3>
+              지금부터 우리 동물 관리는 <br /> 펫북과 함께해요!
+            </h3>
+            <div className="submit_sucess_img"></div>
+            <div className="main_color_button" onClick={() => router.push("/")}>
+              홈으로
+            </div>
+          </div>
+        ) : (
+          <div className="formWrap">
+            <ValidationInput current={"이메일"} />
+            <ValidationInput current={"인증번호"} />
+            {/* <div className="flex">
             <ValidationInput current={"이름"} />
             <ValidationInput current={"생년월일"} />
-          </div>
-          <ValidationInput current={"닉네임"} />
-          <ValidationInput current={"동물종류"} />
-          <ValidationInput current={"인증번호"} />
-          <SubmitBtn>
-            <a className="submitBtn" href="">
+          </div> */}
+            <ValidationInput current={"닉네임"} />
+            <ValidationInput current={"동물종류"} />
+            {/* <ValidationInput current={"인증번호"} /> */}
+            <SubmitBtn onClick={Sign} className="submitBtn">
               회원가입
-            </a>
-          </SubmitBtn>
-        </div>
+            </SubmitBtn>
+          </div>
+        )}
       </Main>
     </>
   );
