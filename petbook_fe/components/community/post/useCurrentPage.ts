32fbca6e1098d2acdc7205bbd8c4df18725a32a7
Eunbi Kang <jenabill@naver.com>
2022-09-25 13:30:29 +0900

    refactor: useCurrentPage hook

--- petbook_fe/components/community/post/useCurrentPage.ts
+++ petbook_fe/components/community/post/useCurrentPage.ts
@@ -1,33 +1,15 @@
 import { useRouter } from "next/router";
-import { useEffect } from "react";
-import { useSelect } from "../useSelect";
 
-export const useCurrentPage = (numPages: number) => {
+export const useCurrentPage = () => {
   const router = useRouter();
-  const [currentPage, selectPage] = useSelect(1);
+  const currentPage = Number(router.query?.page);
   const changeCurrentPage = (page: number) => {
     const url = `/community?page=${page}`;
-    router
-      .push(url, url, {
-        scroll: false,
-      })
-      .then((data) => {
-        if (data) {
-          selectPage(page);
-        }
-      });
+    router.push(url, url, {
+      scroll: false,
+      shallow: true,
+    });
   };
-
-  useEffect(() => {
-    const page = Number(router.query?.page);
-    if (isNaN(page) || page < 1) {
-      changeCurrentPage(1);
-    } else if (page > numPages) {
-      changeCurrentPage(numPages);
-    } else {
-      selectPage(page);
-    }
-  }, [router.query?.page]);
 
   return {
     currentPage,
