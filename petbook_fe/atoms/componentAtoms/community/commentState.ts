0249ab94f4189711e98c57d3b4bd5cb0925fe77d
Eunbi Kang <jenabill@naver.com>
2022-10-03 15:50:13 +0900

    refactor: CommentState interface

--- petbook_fe/atoms/componentAtoms/community/commentState.ts
+++ petbook_fe/atoms/componentAtoms/community/commentState.ts
@@ -1,8 +1,14 @@
 import { atom } from "recoil";
 
-export const commentState = atom<{ comment: string }>({
+export interface CommentState {
+  comment: string;
+  id?: number;
+}
+
+export const commentState = atom<CommentState>({
   key: "commentState",
   default: {
     comment: "",
+    id: undefined,
   },
 });
