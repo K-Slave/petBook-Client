066181a4d8afa5ff98f5e592bd29d7c03b5c0b3a
Eunbi Kang <jenabill@naver.com>
2022-10-03 15:38:39 +0900

    add: WriteComment component

---
+++ petbook_fe/components/community/comment/CommentForm/WriteComment.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+import { useChangeComment } from "./useChangeComment";
+
+interface WriteCommentProps {
+  initialComment: string;
+}
+
+const WriteComment = ({ initialComment }: WriteCommentProps) => {
+  const { onChange } = useChangeComment();
+  return (
+    <Textarea placeholder="당신의 의견을 남겨보세요!" onChange={onChange}>
+      {initialComment}
+    </Textarea>
+  );
+};
+
+export default WriteComment;
+
+const Textarea = styled.textarea`
+  border: none;
+  resize: none;
+  width: 100%;
+  font-size: 14px;
+  line-height: 17px;
+  font-family: Pretendard, sans-serif;
+  &:focus {
+    outline: none;
+  }
+  &::placeholder {
+    color: #b1b1b1;
+  }
+  &::-moz-placeholder {
+    color: #b1b1b1;
+  }
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
