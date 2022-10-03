1d5f85e9bc0df8eb2cbddff0060d518da5e487d1
Eunbi Kang <jenabill@naver.com>
2022-10-03 16:30:25 +0900

    refactor: useSubmitButton hook

--- petbook_fe/components/community/comment/CommentForm/useSubmitComment.ts
+++ petbook_fe/components/community/comment/CommentForm/useSubmitComment.ts
@@ -3,14 +3,12 @@ import { useSetRecoilState } from "recoil";
 import {
   CommentState,
   commentState,
+  initialCommentState,
 } from "../../../../atoms/componentAtoms/community/commentState";
 
 export const useSubmitComment = () => {
   const setComment = useSetRecoilState(commentState);
-  const [input, setInput] = useState<CommentState>({
-    comment: "",
-    id: undefined,
-  });
+  const [input, setInput] = useState<CommentState>(initialCommentState);
   const onSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     setComment((comment) => {
@@ -25,6 +23,11 @@ export const useSubmitComment = () => {
       // patch api
     } else {
       // post api
+      if (input.parentId !== undefined) {
+        // 새로운 대댓글
+      } else {
+        // 새로운 댓글
+      }
     }
     console.log(input);
   }, [input]);
