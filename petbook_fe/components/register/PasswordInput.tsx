86befd2b67fe08fa46b5b25955649dda9617d20f
jj2084jj <whdms3368@gmail.com>
2022-10-08 21:48:39 +0900

    add passinput ui box

---
+++ petbook_fe/components/register/PasswordInput.tsx
@@ -0,0 +1,11 @@
+import ValidationInput from "@components/common/ValidationInput";
+
+const PasswordInput = () => {
+  return (
+    <>
+      <ValidationInput current={"비밀번호"} />
+    </>
+  );
+};
+
+export default PasswordInput;
