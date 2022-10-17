b7a527d069645755f0a3063865af7ea5424d9fe4
Eunbi Kang <jenabill@naver.com>
2022-10-17 23:31:31 +0900

    fix: pagination

--- petbook_fe/components/community/post/useCurrentPage.ts
+++ petbook_fe/components/community/post/useCurrentPage.ts
@@ -1,15 +1,33 @@
 import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useSelect } from "../useSelect";
 
-export const useCurrentPage = () => {
+export const useCurrentPage = (numPages: number) => {
   const router = useRouter();
-  const currentPage = Number(router.query?.page);
+  const [currentPage, selectPage] = useSelect(1);
   const changeCurrentPage = (page: number) => {
     const url = `/community?page=${page}`;
-    router.push(url, url, {
-      scroll: false,
-      shallow: true,
-    });
+    router
+      .push(url, url, {
+        scroll: false,
+      })
+      .then((data) => {
+        if (data) {
+          selectPage(page);
+        }
+      });
   };
+
+  useEffect(() => {
+    const page = Number(router.query?.page);
+    if (isNaN(page) || page < 1) {
+      changeCurrentPage(1);
+    } else if (page > numPages) {
+      changeCurrentPage(numPages);
+    } else {
+      selectPage(page);
+    }
+  }, [router.query?.page]);
 
   return {
     currentPage,
