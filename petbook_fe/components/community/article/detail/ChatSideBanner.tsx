b22c89086714cfc98abd0f567dad816d608b5495
Eunbi Kang <jenabill@naver.com>
2022-10-23 23:48:17 +0900

    fix: css convention

--- petbook_fe/components/community/article/detail/ChatSideBanner.tsx
+++ petbook_fe/components/community/article/detail/ChatSideBanner.tsx
@@ -6,10 +6,10 @@ const avatar =
   "https://images.unsplash.com/photo-1518796745738-41048802f99a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFiYml0fGVufDB8fDB8fA%3D%3D&w=1000&q=80";
 
 const Wrapper = styled.div`
+  flex-shrink: 0;
   position: relative;
   width: 197px;
   border-radius: 16px;
-  flex-shrink: 0;
   background: #ff6e4e;
   img {
     height: 197px;
@@ -18,19 +18,18 @@ const Wrapper = styled.div`
     border-top-right-radius: 16px;
   }
   a {
-    border-bottom: 1px solid #fff;
     align-self: center;
     padding-bottom: 2px;
+    border-bottom: 1px solid #fff;
   }
 `;
 
 const Bottom = styled.div`
-  padding: 0 16px;
   display: flex;
   flex-direction: column;
-  color: #fff;
   gap: 30px;
-  padding-bottom: 30px;
+  padding: 0 16px 30px;
+  color: #fff;
 `;
 
 const Background = styled.div`
