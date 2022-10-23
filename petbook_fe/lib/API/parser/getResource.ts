a21b4a4c467b9f2d9477da70e0d77f07ce8d698f
Eunbi Kang <jenabill@naver.com>
2022-10-24 02:47:08 +0900

    refactor: getResource function

--- petbook_fe/lib/API/parser/getResource.ts
+++ petbook_fe/lib/API/parser/getResource.ts
@@ -12,6 +12,10 @@ export default async function getResource(
   searchParams: URLSearchParams,
   client: QueryClient
 ) {
+  let initUrl = "";
+  if (typeof window === "undefined") {
+    initUrl = process.env.NEXT_PUBLIC_SPR_URL as string;
+  }
   switch (resource.key) {
     case "ARTICLE_LIST": {
       const pageParam = searchParams.get("page");
@@ -20,7 +24,7 @@ export default async function getResource(
 
       await client.fetchQuery(`${resource.key}_${page}_${categoryId}`, () =>
         resource.fetcher({
-          initUrl: process.env.NEXT_PUBLIC_SPR_URL as string,
+          initUrl,
           categoryId: "",
           page: Number(page) - 1,
           size: 10,
@@ -42,7 +46,7 @@ export default async function getResource(
 
       await client.fetchQuery(`${resource.key}_${page}`, () =>
         resource.fetcher({
-          initUrl: process.env.NEXT_PUBLIC_SPR_URL as string,
+          initUrl,
         })
       );
       break;
