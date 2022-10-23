8abd3460b4ec86de2a6e3aa35ab710e892a2b336
Eunbi Kang <jenabill@naver.com>
2022-10-24 02:44:15 +0900

    refactor: ArticleSection component에 api 연결

--- petbook_fe/components/community/article/detail/ArticleSection.tsx
+++ petbook_fe/components/community/article/detail/ArticleSection.tsx
@@ -1,3 +1,6 @@
+import { ARTICLE_ITEM } from "@pages/community/[articleId]";
+import { useRouter } from "next/router";
+import { useQuery } from "react-query";
 import styled from "styled-components";
 import DetailCommonInfo from "../../DetailCommonInfo";
 import TagList from "../../TagList";
@@ -58,36 +61,29 @@ const Bottom = styled.div`
   }
 `;
 
-const avatar =
-  "https://images.unsplash.com/photo-1518796745738-41048802f99a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFiYml0fGVufDB8fDB8fA%3D%3D&w=1000&q=80";
-
-const dummyImage1 =
-  "https://images.unsplash.com/photo-1607599193024-de4a7601aefc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80";
-const dummyImage2 =
-  "https://images.unsplash.com/photo-1629898569904-669319348211?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80";
-const dummyImage3 =
-  "https://images.unsplash.com/photo-1612267168669-679c961c5b31?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80";
+const dummyImages = [
+  "https://images.unsplash.com/photo-1518796745738-41048802f99a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFiYml0fGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+  "https://images.unsplash.com/photo-1607599193024-de4a7601aefc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  "https://images.unsplash.com/photo-1629898569904-669319348211?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+  "https://images.unsplash.com/photo-1612267168669-679c961c5b31?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+];
 
 const ArticleSection = () => {
-  const dummy = {
-    id: 1,
-    title: "제목제목1",
-    content: "본문본문 1",
-    user: {
-      id: 1,
-      nickname: "테스터",
-    },
-    category: {
-      id: 1,
-      name: "질문과 답변",
-    },
-    tags: [],
-    stat: {
-      viewCount: 7,
-      likeCount: 0,
-    },
-  };
-  const { id, title, content, user, category, tags, stat } = dummy;
+  const router = useRouter();
+  const articleId = router.query.articleId as string;
+  const { data } = useQuery(
+    `${ARTICLE_ITEM.key}_${articleId}`,
+    () => ARTICLE_ITEM.fetcher(articleId),
+    {
+      staleTime: Infinity,
+      refetchOnWindowFocus: false,
+    }
+  );
+  if (data === undefined) {
+    return <Section />;
+  }
+
+  const { id, title, content, user, category, tags, stat } = data.data;
   return (
     <Section>
       <Row>
@@ -103,10 +99,10 @@ const ArticleSection = () => {
       </Row>
       <Title>{title}</Title>
       <Content>{content}</Content>
-      <ImageSlider images={[avatar, dummyImage1, dummyImage2, dummyImage3]} />
+      <ImageSlider images={dummyImages} />
       <TagList tags={tags} width={90} height={32} fontSize={16} />
       <Bottom>
-        <span>관람 수 ${stat.viewCount}</span>
+        <span>관람 수 {stat.viewCount}</span>
         <button type="button">좋아요 버튼</button>
         <button type="button">스크랩</button>
       </Bottom>
