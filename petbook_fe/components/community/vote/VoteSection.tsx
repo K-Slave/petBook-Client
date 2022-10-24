bed61c77427e06e3a76b6e5d0cc1d20cfd2d3a2c
Eunbi Kang <jenabill@naver.com>
2022-10-24 17:25:29 +0900

    fix: import 경로 수정

--- petbook_fe/components/community/vote/VoteSection.tsx
+++ petbook_fe/components/community/vote/VoteSection.tsx
@@ -1,11 +1,14 @@
-import MainSection from "../MainSection";
+import CommunitySection from "../CommunitySection";
 import VoteList from "./VoteList";
 
 const VoteSection = () => {
   return (
-    <MainSection title="Talk" description="지금 HOT한 투표! 당신의 선택은?">
+    <CommunitySection
+      title="Talk"
+      description="지금 HOT한 투표! 당신의 선택은?"
+    >
       <VoteList />
-    </MainSection>
+    </CommunitySection>
   );
 };
 
