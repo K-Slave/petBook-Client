4aa5dbb85188236784e3064387cd4c797e47e62b
Eunbi Kang <jenabill@naver.com>
2022-11-03 00:29:22 +0900

    refactor: useButtonOffset hook

--- petbook_fe/lib/hooks/useButtonOffset.ts
+++ petbook_fe/lib/hooks/useButtonOffset.ts
@@ -14,6 +14,6 @@ export default function useButtonOffset({
     } else if (currentPage < offset) {
       setOffset((oldOffset) => oldOffset - btnNum);
     }
-  }, [currentPage, offset]);
+  }, [currentPage]);
   return offset;
 }
