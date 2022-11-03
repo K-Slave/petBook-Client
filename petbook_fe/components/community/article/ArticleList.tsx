3fecd1ed90bad1d04bf75c36840d86a734cb5c74
Eunbi Kang <jenabill@naver.com>
2022-11-03 23:46:05 +0900

    refactor: ArticleList component

--- petbook_fe/components/community/article/ArticleList.tsx
+++ petbook_fe/components/community/article/ArticleList.tsx
@@ -1,4 +1,3 @@
-import styled from "styled-components";
 import { useRef } from "react";
 import { useRecoilValue } from "recoil";
 import categoryState from "@atoms/pageAtoms/community/categoryState";
@@ -6,7 +5,16 @@ import { usePage } from "@lib/hooks/usePagination";
 import useResource from "@lib/hooks/useResource";
 import { ARTICLE_LIST } from "@pages/community";
 import localConsole from "@lib/utils/localConsole";
-import ArticleItem from "./ArticleItem";
+import { ArticleResponse } from "@lib/API/petBookAPI/types/articleRequest";
+import Link from "next/link";
+import TagList from "../TagList";
+import {
+  ArticleListDiv,
+  ArticleItemImg,
+  ArticleItemDiv,
+  ArticleItemAvatar,
+  Article,
+} from "./styled/styledArticleList";
 
 const ArticleList = () => {
   const limit = useRef(10);
@@ -29,16 +37,48 @@ const ArticleList = () => {
   return (
     <ArticleListDiv>
       {posts.map((post) => (
-        <ArticleItem post={post} key={post.id} />
+        <ArticleList.Item post={post} key={post.id} />
       ))}
     </ArticleListDiv>
   );
 };
 
-const ArticleListDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 12px;
-`;
+// ----------------------------------------------------------------------
+
+interface ItemProps {
+  post: ArticleResponse;
+}
+
+const Item = ({ post }: ItemProps) => {
+  const { id, title, content, user, tags, stat } = post;
+  const previewImage = "";
+  const date = "2022-02-03";
+  return (
+    <Link href={`/community/${id}`} passHref>
+      <Article>
+        <div className="ArticleItem_column">
+          <div className="ArticleItem_userInfo">
+            <ArticleItemAvatar />
+            <p className="ArticleItem_nickname">{user.nickname}</p>
+            <p className="ArticleItem_date">{date}</p>
+          </div>
+          <ArticleItemDiv>
+            <h3>{title}</h3>
+            <p className="ArticleItem_content">{content}</p>
+            <TagList tags={tags} width={65} height={26} fontSize={14} />
+            <p className="ArticleItem_stats">
+              <span>공감수 {stat.likeCount}</span>
+              <span>/</span>
+              <span>클릭수 {stat.viewCount}</span>
+            </p>
+          </ArticleItemDiv>
+        </div>
+        {previewImage === "" ? null : <ArticleItemImg />}
+      </Article>
+    </Link>
+  );
+};
+
+ArticleList.Item = Item;
 
 export default ArticleList;
