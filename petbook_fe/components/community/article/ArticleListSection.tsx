8069d2434a52da11366d17ea06a988e69a82075e
Eunbi Kang <jenabill@naver.com>
2022-11-03 23:47:47 +0900

    refactor: ArticleListSection component

--- petbook_fe/components/community/article/ArticleListSection.tsx
+++ petbook_fe/components/community/article/ArticleListSection.tsx
@@ -1,9 +1,7 @@
-import styled from "styled-components";
 import CommunitySection from "../CommunitySection";
 import ArticleList from "./ArticleList";
-import PaginationButton from "./PaginationButton";
-import CategoryFilter from "./CategoryFilter";
-import SortFilter from "./SortFilter";
+import PageButtonBox from "./PageButtonBox";
+import ArticleFilter from "./ArticleFilter";
 
 const ArticleListSection = () => {
   return (
@@ -11,26 +9,11 @@ const ArticleListSection = () => {
       title="Live talk"
       description="실시간 업로드되는 유저들의 이야기"
     >
-      <ArticleListSectionDiv>
-        <ArticleListSection.CategoryFilter /> {/** categoryState atom 공유 */}
-        <ArticleListSection.SortFilter />
-      </ArticleListSectionDiv>
-      <ArticleListSection.List /> {/** categoryState atom 공유 */}
-      <ArticleListSection.ButtonBox />
+      <ArticleFilter />
+      <ArticleList />
+      <PageButtonBox />
     </CommunitySection>
   );
 };
-
-ArticleListSection.CategoryFilter = CategoryFilter;
-ArticleListSection.SortFilter = SortFilter;
-ArticleListSection.List = ArticleList;
-ArticleListSection.ButtonBox = PaginationButton;
-
-const ArticleListSectionDiv = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 32px;
-`;
 
 export default ArticleListSection;
