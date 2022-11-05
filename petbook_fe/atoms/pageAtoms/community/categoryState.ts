e02c5c6aba17e5ab2850ca7f0b01bde0816bbc2f
Eunbi Kang <jenabill@naver.com>
2022-11-05 14:34:50 +0900

    refactor: categoryState atom type

--- petbook_fe/atoms/pageAtoms/community/categoryState.ts
+++ petbook_fe/atoms/pageAtoms/community/categoryState.ts
@@ -1,6 +1,7 @@
+import { CategoryItem } from "@lib/API/petBookAPI/types/categoryRequestSpr";
 import { atom } from "recoil";
 
-const categoryState = atom<{ id: number; name: string }>({
+const categoryState = atom<CategoryItem>({
   key: "categoryState",
   default: {
     id: 0,
