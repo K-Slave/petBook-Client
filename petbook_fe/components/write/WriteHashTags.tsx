40155a05d4dc6595a99ecc054f2e63c3d04dec92
steven-yn <steven_yoon1009@naver.com>
2022-11-05 21:33:39 +0900

    refactor

--- petbook_fe/components/write/WriteHashTags.tsx
+++ petbook_fe/components/write/WriteHashTags.tsx
@@ -1,48 +1,31 @@
-import styled from "styled-components";
-
-const WriteHashTagsSection = styled.section`
-  display: flex;
-  flex-direction: column;
-  gap: 14px;
-
-  width: 100%;
-  height: 103px;
-
-  margin-top: 54px;
-
-  .Hash__Tag__Title {
-    font-weight: bold;
-  }
-`;
-
-const WriteFormInput = styled.input`
-  width: 100%;
-  max-width: 1064px;
-  height: 70px;
-
-  padding: 12px 15px;
-
-  border: 1px solid #f5edde;
-  box-sizing: border-box;
-  border-radius: 12px;
-
-  &::placeholder {
-    pointer-events: none;
-
-    color: #999999;
-  }
-`;
+import {
+  WriteFormInput,
+  WriteHashTagsSection,
+} from "./styled/styledWriteHashTags";
 
 const WriteHashTags = () => {
   return (
     <WriteHashTagsSection>
-      <label className='Hash__Tag__Title'>해시태그</label>
-      <WriteFormInput
-        className='Hash__Tag__Input'
-        placeholder='# 입력 (최대 5개)'
-      />
+      <WriteHashTags.Title />
+      <WriteHashTags.Input />
     </WriteHashTagsSection>
   );
 };
+
+const Title = () => {
+  return <p className="Hash__Tag__Title">해시태그</p>;
+};
+
+const Input = () => {
+  return (
+    <WriteFormInput
+      className="Hash__Tag__Input"
+      placeholder="# 입력 (최대 5개)"
+    />
+  );
+};
+
+WriteHashTags.Title = Title;
+WriteHashTags.Input = Input;
 
 export default WriteHashTags;
