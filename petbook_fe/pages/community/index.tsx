9a38c44088b0197df4ae848927653e6a2e124f32
Eunbi Kang <jenabill@naver.com>
2022-11-05 14:55:02 +0900

    refactor: community page

--- petbook_fe/pages/community/index.tsx
+++ petbook_fe/pages/community/index.tsx
@@ -37,16 +37,14 @@ const Community: NextPage = () => {
   return (
     <CommunityMain>
       <AboutSection />
-      <CommunityDiv>
-        <CommunitySection
-          title="Live talk"
-          description="실시간 업로드되는 유저들의 이야기"
-        >
-          <ArticleFilter />
-          <ArticleList />
-          <PageButtonBox />
-        </CommunitySection>
-      </CommunityDiv>
+      <CommunitySection
+        title="Live talk"
+        description="실시간 업로드되는 유저들의 이야기"
+      >
+        <ArticleFilter />
+        <ArticleList />
+        <PageButtonBox />
+      </CommunitySection>
       <WriteButton />
     </CommunityMain>
   );
