6454cf4f8aea9f842d470a37e023594d52f41f26
Eunbi Kang <jenabill@naver.com>
2022-11-15 00:01:38 +0900

    refactor: add textareaRef in CommentForm component

--- petbook_fe/components/community/comment/CommentForm.tsx
+++ petbook_fe/components/community/comment/CommentForm.tsx
@@ -1,7 +1,5 @@
-import {
-  CommentState,
-} from "@atoms/pageAtoms/community/commentState";
 import useCommentForm from "@lib/hooks/comment/useCommentForm";
+import { useRef } from "react";
 import {
   CommentFormBox,
   CommentFormButton,
@@ -13,12 +11,9 @@ const avatar =
   "https://images.unsplash.com/photo-1518796745738-41048802f99a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFiYml0fGVufDB8fDB8fA%3D%3D&w=1000&q=80";
 const username = "arin";
 
-interface Props {
-  initialContent?: CommentState["content"];
-}
-
-const CommentForm = ({ initialContent } : Props) => {
-  const { onChange, onSubmit } = useCommentForm(initialContent || "");
+const CommentForm = ({ initialContent } : { initialContent?: string; }) => {
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const { onChange, onSubmit } = useCommentForm({ initialContent: initialContent || "", textareaRef });
   return (
     <CommentFormBox>
       <CommentFormDiv>
@@ -29,6 +24,7 @@ const CommentForm = ({ initialContent } : Props) => {
         placeholder="당신의 의견을 남겨보세요!"
         onChange={onChange}
         defaultValue={initialContent}
+        ref={textareaRef}
       />
       <CommentFormButton type="submit" onClick={onSubmit}>
         등록
