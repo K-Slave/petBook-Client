78f8d972945abd1eeede4f0f1367443aab425f59
Eunbi Kang <jenabill@naver.com>
2022-11-14 23:38:53 +0900

    refactor: add comment_create request

--- petbook_fe/pages/community/[articleId].tsx
+++ petbook_fe/pages/community/[articleId].tsx
@@ -4,10 +4,36 @@ import HtmlHeader from "@components/common/HtmlHeader";
 import ChatSideBanner from "@components/community/article/detail/ChatSideBanner";
 import ArticleSection from "@components/community/article/detail/ArticleSection";
 import ImageSliderModal from "@components/community/article/detail/ImageSliderModal";
-import { articleRequest } from "@lib/API/petBookAPI";
-import { createResource } from "@lib/hooks/common/useResource";
+import { articleRequest, commentRequest } from "@lib/API/petBookAPI";
+import { createRequest, createResource } from "@lib/hooks/common/useResource";
 import CommentList from "@components/community/comment/CommentList";
 import CommentForm from "@components/community/comment/CommentForm";
+
+export const ARTICLE_ITEM = {
+  key: "ARTICLE_ITEM",
+  fetcher: articleRequest.article_item,
+};
+
+export const COMMENT_CREATE = createRequest({
+  key: "COMMENT_CREATE",
+  requester: commentRequest.comment_create
+});
+
+const ArticleDetail: NextPage = () => {
+  return (
+    <>
+      <HtmlHeader />
+      <ArticleDetailMain>
+        <ArticleSection />
+        <CommentSection>
+          <CommentList />
+          <CommentForm />
+        </CommentSection>
+      </ArticleDetailMain>
+      <ImageSliderModal />
+    </>
+  );
+};
 
 const ArticleDetailMain = styled.main`
   display: flex;
@@ -25,27 +51,6 @@ const CommentSection = styled.section`
   flex-direction: column;
   gap: 8px;
 `;
-
-export const ARTICLE_ITEM = {
-  key: "ARTICLE_ITEM",
-  fetcher: articleRequest.article_item,
-};
-
-const ArticleDetail: NextPage = () => {
-  return (
-    <>
-      <HtmlHeader />
-      <ArticleDetailMain>
-        <ArticleSection />
-        <CommentSection>
-          <CommentList />
-          <CommentForm />
-        </CommentSection>
-      </ArticleDetailMain>
-      <ImageSliderModal />
-    </>
-  );
-};
 
 type PetbookPages = NextPage & {
   requiredResources?: [typeof ARTICLE_ITEM];
