f784f03d0c5a33003f0c8a328e4f6895fa5c776f
jj2084jj <whdms3368@gmail.com>
2022-11-14 22:51:03 +0900

    fixed registerPassword from ESlint error

--- petbook_fe/components/register/RegisterPasswordFrom.tsx
+++ petbook_fe/components/register/RegisterPasswordFrom.tsx
@@ -6,11 +6,11 @@ const PasswordInput = () => {
   const PassCheckType = [
     {
       type: "영어 대문자 포함",
-      value: 1,
+      value: "english",
       check: true,
     },
-    { type: "특수문자 포함", value: 2, check: false },
-    { type: "8자 이상", value: 3, check: false },
+    { type: "특수문자 포함", value: "spacial", check: false },
+    { type: "8자 이상", value: "limit", check: false },
   ];
   return (
     <div>
@@ -20,10 +20,10 @@ const PasswordInput = () => {
         current="비밀번호"
       />
       <PassCheckWrap>
-        {PassCheckType.map((item, index: number) => {
+        {PassCheckType.map((item) => {
           const { type } = item;
           return (
-            <li key={index}>
+            <li key={item.value}>
               <input type="radio" id={item.value} checked={item.check} />
               <label
                 className={item.check ? "active" : ""}
