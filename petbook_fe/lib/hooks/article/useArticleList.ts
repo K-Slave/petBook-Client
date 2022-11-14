0a0467780853c9ab1ed2fdfb3f229b6b9ea7dbfd
Eunbi Kang <jenabill@naver.com>
2022-11-14 16:01:10 +0900

    refactor: useArticleList hook

--- petbook_fe/lib/hooks/article/useArticleList.ts
+++ petbook_fe/lib/hooks/article/useArticleList.ts
@@ -2,17 +2,19 @@ import categoryState from "@atoms/pageAtoms/community/categoryState";
 import { ARTICLE_LIST } from "@pages/community";
 import { useRef } from "react";
 import { useRecoilValue } from "recoil";
-import { ArticleListResponse } from "@lib/API/petBookAPI/types/articleRequest";
+import { ArticleItem } from "@lib/API/petBookAPI/types/articleRequest";
 import { usePage } from "./usePagination";
 import useResource from "../common/useResource";
 
-export default function useArticleList(): Pick<ArticleListResponse, "articles"> & {
-  totalPages: number
+export default function useArticleList(): {
+  status: "idle" | "error" | "loading" | "success";
+  articles: ArticleItem[];
+  totalPages: number;
 } {
     const size = useRef(10);
     const category = useRecoilValue(categoryState);
     const page = usePage();
-    const { data } = useResource({
+    const { data, status } = useResource({
       key: `${ARTICLE_LIST.key}_${page}_${category.id}`,
       fetcher: ARTICLE_LIST.fetcher,
       params: {
@@ -21,12 +23,10 @@ export default function useArticleList(): Pick<ArticleListResponse, "articles"> 
         size: size.current,
       }
     });
-    const articleList = data === undefined ? {
-      articles: [],
-      totalPages: 0
-    } : {
-      articles: data.data.articles,
-      totalPages: Math.ceil(data.data.totalElements / size.current)
+    const articleList = {
+      status,
+      articles: data === undefined ? [] : data.data.articles,
+      totalPages: data === undefined ? 0 : Math.ceil(data.data.totalElements / size.current)
     };
     return articleList;
 }
