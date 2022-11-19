67f0425f51a89716e3f75888dc449113c8493a74
Eunbi Kang <jenabill@naver.com>
2022-11-19 11:35:30 +0900

    refactor: CommentList component

--- petbook_fe/components/community/comment/CommentForm.tsx
+++ petbook_fe/components/community/comment/CommentForm.tsx
@@ -11,9 +11,14 @@ const avatar =
   "https://images.unsplash.com/photo-1518796745738-41048802f99a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFiYml0fGVufDB8fDB8fA%3D%3D&w=1000&q=80";
 const username = "arin";
 
-const CommentForm = ({ initialContent } : { initialContent?: string; }) => {
+const CommentForm = ({ initialContent }: { initialContent?: string }) => {
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
-  const { onChange, onSubmit } = useCommentForm({ initialContent: initialContent || "", textareaRef });
+  const { onChange, onSubmit, isCreateSuccess, isUpdateSuccess } = useCommentForm({
+    initialContent: initialContent || "",
+    textareaRef,
+  });
+  // console.log(isCreateSuccess);
+  // console.log(isUpdateSuccess);
   return (
     <CommentFormBox>
       <CommentFormDiv>
