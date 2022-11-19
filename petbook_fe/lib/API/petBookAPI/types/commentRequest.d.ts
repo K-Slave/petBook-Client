a2dbcc8bc2a9ea504c92d91e491c9ceabf041c9e
Eunbi Kang <jenabill@naver.com>
2022-11-19 10:50:47 +0900

    update comment request & response types

--- petbook_fe/lib/API/petBookAPI/types/commentRequest.d.ts
+++ petbook_fe/lib/API/petBookAPI/types/commentRequest.d.ts
@@ -1,7 +1,29 @@
 export interface CommentCreateRequest {
-    content: string;
-    articleId: number;
-    parentId: number | null;
+  content: string;
+  articleId: number;
+  parentId: number | null;
 }
 
 export type CommentUpdateRequest = Pick<CommentCreateRequest, "content">;
+
+export interface CommentItem {
+  id: number;
+  content: string;
+  user: {
+    id: number;
+    nickname: string;
+  };
+  articleId: number;
+  likeCount: number;
+  createdAt: string;
+}
+
+export type CommentListResponse = {
+  parent: CommentItem;
+  children: CommentItem[];
+}[];
+
+export interface CommentListRequest {
+  params: { articleId: number };
+  headerObj?: object;
+}
