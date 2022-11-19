a701c1f5126623c8a793ee0dccc9baa58fff350b
Eunbi Kang <jenabill@naver.com>
2022-11-19 11:36:33 +0900

    add COMMENT_DELETE resource

--- petbook_fe/pages/community/[articleId].tsx
+++ petbook_fe/pages/community/[articleId].tsx
@@ -14,15 +14,25 @@ export const ARTICLE_ITEM = {
   fetcher: articleRequest.article_item,
 };
 
+export const COMMENT_LIST = {
+  key: "COMMENT_LIST",
+  fetcher: commentRequest.comment_list,
+};
+
 export const COMMENT_CREATE = createRequest({
   key: "COMMENT_CREATE",
   requester: commentRequest.comment_create,
 });
 
-export const COMMENT_LIST = {
-  key: "COMMENT_LIST",
-  fetcher: commentRequest.comment_list,
-};
+export const COMMENT_UPDATE = createRequest({
+  key: "COMMENT_UPDATE",
+  requester: commentRequest.comment_update
+});
+
+export const COMMENT_DELETE = createRequest({
+  key: "COMMENT_DELETE",
+  requester: commentRequest.comment_delete
+});
 
 const ArticleDetail: NextPage = () => {
   return (
