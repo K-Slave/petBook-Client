44698d5213474b8ebc58ad574b9d9f1f55e8c151
Eunbi Kang <jenabill@naver.com>
2022-11-19 11:36:12 +0900

    update useCommentForm hook (add comment update api)

--- petbook_fe/lib/hooks/comment/useCommentForm.ts
+++ petbook_fe/lib/hooks/comment/useCommentForm.ts
@@ -1,46 +1,72 @@
 import commentState from "@atoms/pageAtoms/community/commentState";
-import localConsole from "@lib/utils/localConsole";
-import { COMMENT_CREATE } from "@pages/community/[articleId]";
+import { COMMENT_CREATE, COMMENT_UPDATE } from "@pages/community/[articleId]";
 import { useRouter } from "next/router";
 import { MutableRefObject, useEffect } from "react";
 import { useSetRecoilState } from "recoil";
 import { useSetResource } from "../common/useResource";
 
-export default function useCommentForm({ initialContent, textareaRef } : {
-  initialContent: string,
-  textareaRef: MutableRefObject<HTMLTextAreaElement | null>
+export default function useCommentForm({
+  initialContent,
+  textareaRef,
+}: {
+  initialContent: string;
+  textareaRef: MutableRefObject<HTMLTextAreaElement | null>;
 }) {
-    const router = useRouter();
-    const setComment = useSetRecoilState(commentState);
-    const { mutate: createComment, isSuccess: isCreateSuccess } = useSetResource(COMMENT_CREATE);
-    const onChange = (
-      e:
-        | React.ChangeEvent<HTMLInputElement>
-        | React.ChangeEvent<HTMLTextAreaElement>
-    ) => {
-      const { value } = e.target;
-      setComment((comment) => ({ ...comment, content: value }));
-    };
+  const router = useRouter();
+  const setComment = useSetRecoilState(commentState);
+  const { mutate: createComment, isSuccess: isCreateSuccess } =
+    useSetResource(COMMENT_CREATE);
+  const { mutate: updateComment, isSuccess: isUpdateSuccess } = useSetResource(COMMENT_UPDATE);
+  const onChange = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    const { value } = e.target;
+    setComment((comment) => ({ ...comment, content: value }));
+  };
 
-    const onSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-        const { articleId } = router.query as { articleId: string };
-        setComment((comment) => {
-          const { content, commentId, parentId } = comment;
-          localConsole.log(`content: ${content}, commentId: ${commentId || "null"}, parentId: ${parentId || "null"}, articleId: ${articleId}`);
-          if (content === "") return comment;
-          if (commentId === null) {
-            // comment_create
-            // createComment({ content, parentId, articleId: Number(articleId) });
-          } else {
-            // comment_update
-          }
-          if (textareaRef.current != null) textareaRef.current.value = "";
-          return { content: "", commentId: null, parentId: null };
+  const onSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const { articleId } = router.query as { articleId: string };
+    setComment((comment) => {
+      const { content, commentId, parentId } = comment;
+      if (content === "") return comment;
+      if (commentId === null) {
+        /*
+        createComment({
+          body: { content, parentId, articleId: Number(articleId) },
+          headerObj: {
+            Authorization: `Bearer ${String(process.env.NEXT_PUBLIC_TOKEN)}`,
+          },
         });
-    };
-    useEffect(() => {
-        if (initialContent !== "") setComment((comment) => ({ ...comment, content: initialContent }));
-    }, []);
-    return { onChange, onSubmit };
+        */
+      } else {
+        /*
+        updateComment({
+          body: { content },
+          pathParam: String(commentId),
+          headerObj: {
+            Authorization: `Bearer ${String(process.env.NEXT_PUBLIC_TOKEN)}`,
+          },
+        });
+        */
+      }
+      /*
+      console.log(
+        `content: ${content}, commentId: ${commentId || "null"}, parentId: ${
+          parentId || "null"
+        }, articleId: ${articleId}`
+      );
+      */
+      if (textareaRef.current != null) textareaRef.current.value = "";
+      return { content: "", commentId: null, parentId: null };
+    });
+  };
+  useEffect(() => {
+    if (initialContent !== "") {
+      setComment((comment) => ({ ...comment, content: initialContent }));
+    }
+  }, []);
+  return { onChange, onSubmit, isCreateSuccess, isUpdateSuccess };
 }
