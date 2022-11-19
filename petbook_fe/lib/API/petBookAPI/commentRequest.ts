a1108e20e904f3efd0b94ef7b29e4499b5a828d1
Eunbi Kang <jenabill@naver.com>
2022-11-19 10:52:47 +0900

    update CommentAPI methods

--- petbook_fe/lib/API/petBookAPI/commentRequest.ts
+++ petbook_fe/lib/API/petBookAPI/commentRequest.ts
@@ -1,8 +1,12 @@
 import RequestCore from "./RequestCore";
-import { CommentCreateRequest, CommentUpdateRequest } from "./types/commentRequest";
+import {
+  CommentCreateRequest,
+  CommentListResponse,
+  CommentUpdateRequest,
+} from "./types/commentRequest";
 
 export default class CommentAPI extends RequestCore {
-    /**
+  /**
    * @param payload
    * @param payload.body 요청 패킷 Body 에 JSON 형태로 담을 내용입니다.
    * @param payload.body.content 댓글 내용입니다.
@@ -11,25 +15,25 @@ export default class CommentAPI extends RequestCore {
    * @param payload.headerObj 유저 토큰값을 헤더에 작성합니다. 없으면 에러가 납니다.
    * @returns 생성한 댓글 정보가 들어옵니다.
    */
-    public comment_create = async (payload: {
-        body: CommentCreateRequest,
-        headerObj?: object
-    }) => {
-        const { body, headerObj } = payload;
-        const { requestURL, requestHeaders } = this.getParameters({
-            headerObj
-          });
+  public comment_create = async (payload: {
+    body: CommentCreateRequest;
+    headerObj?: object;
+  }) => {
+    const { body, headerObj } = payload;
+    const { requestURL, requestHeaders } = this.getParameters({
+      headerObj,
+    });
 
-          const result = await this.getResult({
-            requestMethod: "POST",
-            requestURL,
-            requestHeaders,
-            body,
-          });
-          return result;
-    };
+    const result = await this.getResult({
+      requestMethod: "POST",
+      requestURL,
+      requestHeaders,
+      body,
+    });
+    return result;
+  };
 
-    /**
+  /**
    *
    * @param payload
    * @param payload.pathParam 댓글 ID값입니다.
@@ -38,50 +42,50 @@ export default class CommentAPI extends RequestCore {
    * @param payload.headerObj 유저 토큰값을 헤더에 작성합니다. 없으면 에러가 납니다.
    * @returns 업데이트된 댓글 정보가 들어옵니다.
    */
-    public comment_update = async (payload: {
-        pathParam: string,
-        body: CommentCreateRequest,
-        headerObj?: object
-    }) => {
-        const { pathParam, body, headerObj } = payload;
-        const { requestURL, requestHeaders } = this.getParameters({
-            pathParam,
-            headerObj
-        });
-        const result = await this.getResult({
-            requestMethod: "PUT",
-            requestURL,
-            requestHeaders,
-            body
-        });
-        return result;
-    };
+  public comment_update = async (payload: {
+    pathParam: string;
+    body: CommentUpdateRequest;
+    headerObj?: object;
+  }) => {
+    const { pathParam, body, headerObj } = payload;
+    const { requestURL, requestHeaders } = this.getParameters({
+      pathParam,
+      headerObj,
+    });
+    const result = await this.getResult({
+      requestMethod: "PUT",
+      requestURL,
+      requestHeaders,
+      body,
+    });
+    return result;
+  };
 
-     /**
+  /**
    *
    * @param payload
    * @param payload.pathParam 댓글 ID값입니다.
    * @param payload.headerObj 유저 토큰값을 헤더에 작성합니다. 없으면 에러가 납니다.
    * @returns 업데이트된 댓글 정보가 들어옵니다.
    */
-    public comment_delete = async (payload: {
-        pathParam: string,
-        headerObj?: object
-    }) => {
-        const { pathParam, headerObj } = payload;
-        const { requestURL, requestHeaders } = this.getParameters({
-            pathParam,
-            headerObj
-        });
-        const result = this.getResult({
-            requestMethod: "DELETE",
-            requestURL,
-            requestHeaders
-        });
-        return result;
-    };
+  public comment_delete = async (payload: {
+    pathParam: string;
+    headerObj?: object;
+  }) => {
+    const { pathParam, headerObj } = payload;
+    const { requestURL, requestHeaders } = this.getParameters({
+      pathParam,
+      headerObj,
+    });
+    const result = this.getResult({
+      requestMethod: "DELETE",
+      requestURL,
+      requestHeaders,
+    });
+    return result;
+  };
 
-     /**
+  /**
    *
    * @param payload
    * @param payload.params 입력할 쿼리파라미터 입니다.
@@ -89,19 +93,20 @@ export default class CommentAPI extends RequestCore {
    * @param config Header 메시지를 추가할때 씁니다.
    * @returns 게시물의 댓글 리스트를 반환합니다.
    */
-    public comment_list = async (payload: {
-        params: { articleId: number },
-        headerObj?: object,
-    }) => {
-        const { headerObj } = payload;
-        const { requestURL, requestHeaders } = this.getParameters({
-            headerObj
-        });
-        const result = this.getResult({
-            requestMethod: "GET",
-            requestURL,
-            requestHeaders
-        });
-        return result;
-    };
+  public comment_list = async (
+    params: { articleId: string },
+    config?: { headerObj: object }
+  ) => {
+    const { requestURL, requestHeaders } = this.getParameters({
+      uri: "/list",
+      headerObj: config && config.headerObj,
+      params,
+    });
+    const result = this.getResult<CommentListResponse>({
+      requestMethod: "GET",
+      requestURL,
+      requestHeaders,
+    });
+    return result;
+  };
 }
