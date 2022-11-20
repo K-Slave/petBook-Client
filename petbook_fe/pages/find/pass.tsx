52da6d29e9b26ce5a15fef972ed20b66a130b92a
jj2084jj <whdms3368@gmail.com>
2022-11-19 20:19:18 -0800

    add find from

---
+++ petbook_fe/pages/find/pass.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+import { FindPassForm } from "@components/find/FindPassForm";
+
+const Main = styled.main`
+  overflow: auto;
+  padding-top: 194px;
+  margin: 0 auto;
+  max-width: 400px;
+  height: calc(100vh - 61px);
+  background-color: var(--bg);
+`;
+
+export const FindUserInfo = () => {
+  return (
+    <Main>
+      <FindPassForm />
+    </Main>
+  );
+};
+export default FindUserInfo;
