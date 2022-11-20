e04408db5a68dc41d78b6fea212c11d6cbea5e27
jj2084jj <whdms3368@gmail.com>
2022-11-19 20:19:18 -0800

    add find from

---
+++ petbook_fe/components/find/FindInputBox.tsx
@@ -0,0 +1,41 @@
+import { registerFormState } from "@atoms/pageAtoms/login/userState";
+import React, { ChangeEventHandler } from "react";
+import { useSetRecoilState } from "recoil";
+
+import { IconBox, InputBox } from "./style/styledFindSubmit";
+
+interface LoginProps {
+  current: string;
+  axiosValue: string;
+  IconType: string;
+}
+
+const FindInputBox = ({ current, axiosValue, IconType }: LoginProps) => {
+  // const setLoginForm = useSetRecoilState(registerFormState);
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    console.log(e.target.value);
+    // setLoginForm((user) => ({
+    //   ...user,
+    //   [`${axiosValue}`]: e.target.value,
+    // }));
+  };
+
+  return (
+    <div>
+      <InputBox>
+        <IconBox>
+          <div className={`${IconType}`} />
+        </IconBox>
+        <label htmlFor={`${current}`}>
+          <input
+            type={`${axiosValue}`}
+            id={`${current}`}
+            placeholder={`${current}를 입력해주세요 `}
+            onChange={onChange}
+          />
+        </label>
+      </InputBox>
+    </div>
+  );
+};
+export default FindInputBox;
