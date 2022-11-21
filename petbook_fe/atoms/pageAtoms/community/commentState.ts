a5d457ca69f9595da2823258439d820e5b5c182d
Eunbi Kang <jenabill@naver.com>
2022-11-21 14:17:28 +0900

    refactor: commentState atom

--- petbook_fe/atoms/pageAtoms/community/commentState.ts
+++ petbook_fe/atoms/pageAtoms/community/commentState.ts
@@ -3,7 +3,7 @@ import { atom } from "recoil";
 interface CommentState {
   content: string;
   commentId: number | null;
-  parentId: number;
+  parentId: number | null;
 }
 
 const commentState = atom<CommentState>({
@@ -11,7 +11,7 @@ const commentState = atom<CommentState>({
   default: {
     content: "",
     commentId: null,
-    parentId: 0,
+    parentId: null,
   },
 });
 
