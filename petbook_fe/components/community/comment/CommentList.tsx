3148e5f1e21659241902aaab015ecf560acaf851
Eunbi Kang <jenabill@naver.com>
2022-11-21 21:41:25 +0900

    fix and refactor: add key to children and add comment delete api

--- petbook_fe/components/community/comment/CommentList.tsx
+++ petbook_fe/components/community/comment/CommentList.tsx
@@ -1,15 +1,24 @@
 import { BsThreeDotsVertical, BsArrowReturnRight } from "react-icons/bs";
-import useResource from "@lib/hooks/common/useResource";
-import { COMMENT_LIST } from "@pages/community/[articleId]";
+import useResource, { useSetResource } from "@lib/hooks/common/useResource";
+import { COMMENT_DELETE, COMMENT_LIST } from "@pages/community/[articleId]";
 import { useRouter } from "next/router";
 import { CommentItem } from "@lib/API/petBookAPI/types/commentRequest";
+import React, { Fragment, MouseEventHandler, useState } from "react";
+import { useQueryClient } from "react-query";
+import { useSetRecoilState } from "recoil";
+import commentState from "@atoms/pageAtoms/community/commentState";
+import useChangeComment from "@lib/hooks/comment/useChangeComment";
+import useSelectorState from "@lib/hooks/common/useSelectorState";
+import useRecoilSelector from "@lib/hooks/common/useRecoilSelector";
 import DetailCommonInfo from "../DetailCommonInfo";
 import { CommentListDiv, ItemDiv } from "./styled/styledCommentList";
+import { CommentFormMain } from "./CommentForm";
 
 const avatar =
   "https://images.unsplash.com/photo-1518796745738-41048802f99a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFiYml0fGVufDB8fDB8fA%3D%3D&w=1000&q=80";
 
 const CommentList = () => {
+  const queryClient = useQueryClient();
   const router = useRouter();
   const articleId = router.query.articleId as string;
   const { data } = useResource({
@@ -19,20 +28,49 @@ const CommentList = () => {
         articleId,
       }),
   });
+  const { mutate: deleteComment } = useSetResource(COMMENT_DELETE);
+  const onDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const commentId = e.currentTarget.dataset.commentid;
+    if (commentId === undefined) {
+      alert("댓글 삭제에 실패했습니다. 다시 시도해주세요 😢");
+    } else {
+      deleteComment(
+        {
+          pathParam: `/${commentId}`,
+        },
+        {
+          onSuccess: async () => {
+            await queryClient.invalidateQueries(
+              `${COMMENT_LIST.key}_${articleId}`
+            );
+          },
+          onError: () => {
+            alert("댓글 삭제에 실패했습니다. 다시 시도해주세요 😢");
+          },
+        }
+      );
+    }
+  };
 
   return (
     <CommentListDiv>
       {data?.data.map((comment) => (
-        <>
+        <Fragment key={comment.parent.id}>
           <CommentList.Item
             comment={comment.parent}
             isChild=""
+            onDelete={onDelete}
             key={comment.parent.id}
           />
           {comment.children.map((child) => (
-            <CommentList.Item comment={child} isChild="true" key={child.id} />
+            <CommentList.Item
+              comment={child}
+              isChild="true"
+              onDelete={onDelete}
+              key={child.id}
+            />
           ))}
-        </>
+        </Fragment>
       ))}
     </CommentListDiv>
   );
@@ -43,9 +81,10 @@ const CommentList = () => {
 interface ItemProps {
   comment: CommentItem;
   isChild: string;
+  onDelete: MouseEventHandler<HTMLButtonElement>;
 }
 
-const Item = ({ comment, isChild }: ItemProps) => {
+const Item = ({ comment, isChild, onDelete }: ItemProps) => {
   const { user, createdAt, content, likeCount, id, articleId } = comment;
   return (
     <ItemDiv isChild={isChild}>
@@ -57,9 +96,12 @@ const Item = ({ comment, isChild }: ItemProps) => {
             date={createdAt.split("T")[0]}
             avatar={avatar}
           />
-          <button type="button">
-            <BsThreeDotsVertical />
-          </button>
+          <div>
+            <button type="button">수정</button>
+            <button type="button" data-commentid={id} onClick={onDelete}>
+              삭제
+            </button>
+          </div>
         </div>
         <p className="Item_Content">{content}</p>
         <div className="Item_Button_Box">
