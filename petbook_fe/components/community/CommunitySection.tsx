92a3db8c9175b5afed0411dee2290b756c56a930
Eunbi Kang <jenabill@naver.com>
2022-12-10 00:55:24 +0900

    design: button tag

--- petbook_fe/components/community/CommunitySection.tsx
+++ petbook_fe/components/community/CommunitySection.tsx
@@ -4,18 +4,22 @@ import styled from "styled-components";
 
 const CommunitySectionBox = styled.section`
   margin-bottom: 56px;
+  color: var(--black_01);
   .heading {
     display: flex;
     align-items: center;
     justify-content: space-between;
     margin-bottom: 28px;
-    color: var(--black_01);
     h3 {
       font-size: 22px;
       font-weight: bold;
       text-transform: uppercase;
     }
-  }   
+  }
+  button {
+    color: var(--black_01);
+    font-size: 14px;
+  }
 `;
 interface Props {
   title: string;
