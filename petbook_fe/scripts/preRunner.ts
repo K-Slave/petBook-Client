0f159e322b8169a43be312102efca32bb22c571b
steven-yn <steven_yoon1009@naver.com>
2022-12-13 02:32:53 +0900

    ✨ ADD : 빌드 로그 텍스트 저장, 디코 웹훅 전송 테스트

--- petbook_fe/scripts/preRunner.ts
+++ petbook_fe/scripts/preRunner.ts
@@ -1,22 +1,10 @@
 import { EmbedBuilder, Events, WebhookClient } from "discord.js";
-import readline from "readline";
 import * as dotenv from "dotenv";
+import { loadEnvConfig } from "@next/env";
+import fs from "fs";
+import path from "path";
 
 dotenv.config();
-
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-
-// rl.on("line", (line) => {
-//   console.log(line, "line");
-//   rl.close();
-// });
-
-// rl.on("close", () => {
-//   process.exit();
-// });
 
 const webhookClient = new WebhookClient({
   url: "https://discord.com/api/webhooks/1044966698060623902/8SuHHBWIkMPt57e2O4jgGrgbbkPZjJNdkSXFRVJlLLX3MAzB5f_JTKnYn69DgAV4-WCH",
@@ -26,23 +14,22 @@ const webhookClient = new WebhookClient({
 // console.log(process.env.NODE_ENV, "NODE_ENV");
 // console.log(process.env.TEST_WEBHOOK_URL, "process.env.TEST_WEBHOOK_URL");
 
-const embed = new EmbedBuilder().setTitle("Hello petBook !").setColor(0x00ffff);
+const embedContents = (content: string) => {
+  return new EmbedBuilder().setTitle(content).setColor(0x00ffff);
+};
 
-// process.on("worker", () => {
-//   console.log("worker evt");
-// });
+const date = new Date();
 
-// process.on("exit", () => {
-//   console.log("exit evt");
-// });
+const time = `${date.getFullYear()}년 ${date.getMonth()}월 ${date.getDay()}일 ${date.getHours()}시 ${date.getMinutes()}분 ${date.getSeconds()}초`;
 
 webhookClient
   .send({
     username: "petBot",
     avatarURL:
       "https://cdn.discordapp.com/app-icons/1044621624864940163/87fe18353f90a7a4c275be945afc14e5.png?size=512",
-    embeds: [embed],
-    content: "hey",
+    embeds: [embedContents(`${time} 부터 넥스트 빌드 시작`)],
+    content: `빌드중... \n시작시간 : ${new Date().toISOString()}
+    `,
   })
   .then((d) => d)
   .catch((e) => e);
