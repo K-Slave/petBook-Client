3400cb68ce367ca8a1e5c49317a925b0ceb0ded9
Eunbi <jenabill@naver.com>
2022-12-15 13:28:37 +0900

    rename: /api/cookie folder

--- petbook_fe/pages/api/cookie/index.ts
+++ petbook_fe/pages/api/cookie/index.ts
@@ -4,7 +4,11 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
         switch (req.method) {
             case 'POST': {
-                res.setHeader('Set-Cookie', `pekBookUser=${String(req.body.token)}; Path=/; SameSite=Strict; Max-Age=2592000; Secure; HttpOnly;`);
+                res.setHeader('Set-Cookie', `${String(req.body.key)}=${String(req.body.value)}; Path=/; SameSite=Strict; Max-Age=2592000; Secure; HttpOnly;`);
+                return res.status(200).json({ success: true });
+            }
+            case "DELETE": {
+                res.setHeader('Set-Cookie', `${String(req.body.key)}=; Path=/; SameSite=Strict;`);
                 return res.status(200).json({ success: true });
             }
             default:
