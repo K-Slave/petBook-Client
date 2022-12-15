f78aeb74a198bfa80b7f3f0713398e0df45fdfe8
Eunbi <jenabill@naver.com>
2022-12-15 16:05:51 +0900

    feature: /api/cookie GET API

--- petbook_fe/pages/api/cookie/index.ts
+++ petbook_fe/pages/api/cookie/index.ts
@@ -3,8 +3,12 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
         switch (req.method) {
+            case "GET": {
+                const value = req.cookies[String(req.query.key)];
+                return res.status(200).json({ value });
+            }
             case 'POST': {
-                res.setHeader('Set-Cookie', `${String(req.body.key)}=${String(req.body.value)}; Path=/; SameSite=Strict; Max-Age=2592000; Secure; HttpOnly;`);
+                res.setHeader('Set-Cookie', `${String(req.body.key)}=${String(req.body.value)}; Path=/; SameSite=Strict; Max-Age=2592000; secure; httpOnly`);
                 return res.status(200).json({ success: true });
             }
             case "DELETE": {
@@ -15,6 +19,6 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
                 break;
         }
     } catch (error: any) {
-        return res.status(500).json({ ...error, message: error.message, verify: false });
+        return res.status(500).json({ ...error, message: error.message, success: false });
     }
 }
