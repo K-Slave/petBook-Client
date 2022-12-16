49c806b0a2dd750bcd721a21cd3391aee2434355
Eunbi <jenabill@naver.com>
2022-12-16 17:16:25 +0900

    add resources to article detail page

--- petbook_fe/pages/community/[articleId].tsx
+++ petbook_fe/pages/community/[articleId].tsx
@@ -8,11 +8,22 @@ import BackButton from "@components/community/BackButton";
 import ArticleContainer from "@containers/ArticleContainer";
 import { sprPetBookClient } from "@lib/API/axios/axiosClient";
 import { getHttpOnlyCookie } from "@lib/utils/httpOnlyCookie";
+import { useEffect } from "react";
 
 export const ARTICLE_ITEM = {
   key: "ARTICLE_ITEM",
   fetcher: articleRequest.article_item,
 };
+
+export const ARTICLE_CREATE_LIKE = createRequest({
+  key: "ARTICLE_CREATE_LIKE",
+  requester: articleRequest.article_create_like
+});
+
+export const ARTICLE_DELETE_LIKE = createRequest({
+  key: "ARTICLE_DELETE_LIKE",
+  requester: articleRequest.article_delete_like
+});
 
 export const COMMENT_LIST = {
   key: "COMMENT_LIST",
@@ -34,6 +45,16 @@ export const COMMENT_DELETE = createRequest({
   requester: commentRequest.comment_delete
 });
 
+export const COMMENT_CREATE_LIKE = createRequest({
+  key: "COMMENT_CREATE_LIKE",
+  requester: commentRequest.comment_create_like
+});
+
+export const COMMENT_DELETE_LIKE = createRequest({
+  key: "COMMENT_DELETE_LIKE",
+  requester: commentRequest.comment_delete_like
+});
+
 interface Props {
   token: string | null;
 }
@@ -42,6 +63,12 @@ type PetbookPage = NextPage<Props> & {
 };
 
 const ArticleDetail: PetbookPage = ({ token }) => {
+  useEffect(() => {
+    if (token) {
+      // sprPetBookClient in client-side
+      sprPetBookClient.defaults.headers.common.Authorization = `Bearer ${token}`;
+    }
+  }, [token]);
   return (
     <>
       <HtmlHeader />
@@ -69,6 +96,7 @@ const ArticleDetailMain = styled.main`
 ArticleDetail.getInitialProps = async (ctx: NextPageContext): Promise<Props> => {
   const token = await getHttpOnlyCookie({ ctx, key: "petBookUser" });
   if (token) {
+    // sprPetBookClient in server-side and client-side
     sprPetBookClient.defaults.headers.common.Authorization = `Bearer ${token}`;
     ArticleDetail.requiredResources = [ARTICLE_ITEM, COMMENT_LIST];
   } else {
