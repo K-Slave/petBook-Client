3330d1137a45849d5d3be907a8aa1915ef9cf29f
steven-yn <steven_yoon1009@naver.com>
2022-12-19 23:54:34 +0900

    FIX: 디코 알림 세부구현

--- petbook_fe/scripts/postRunner.ts
+++ petbook_fe/scripts/postRunner.ts
@@ -2,13 +2,16 @@ import { EmbedBuilder, WebhookClient, AttachmentBuilder } from "discord.js";
 import * as dotenv from "dotenv";
 import fs from "fs";
 import path from "path";
+import envSelector from "./envSelector";
 
 dotenv.config();
+
+const env = envSelector("develop");
 
 console.log("post runner start");
 
 const webhookClient = new WebhookClient({
-  url: process.env.PETBOOK_WEBHOOK_URL as string,
+  url: env.webhookURL,
 });
 
 const buildLogPath = path.join(__dirname, "../buildLog.txt");
@@ -17,14 +20,37 @@ const errorLogPath = path.join(__dirname, "../errorLog.txt");
 const buildLog = fs.readFileSync(buildLogPath, "utf-8");
 const errorLog = fs.readFileSync(errorLogPath, "utf-8");
 const embedContents = (content: string) => {
-  return new EmbedBuilder().setTitle(content);
+  return new EmbedBuilder().setTitle(content).setDescription(`${time}`);
 };
 
 const date = new Date();
+const toISOStringWithTimezone = (dateObj: Date) => {
+  const tzOffset = -date.getTimezoneOffset();
+  const diff = tzOffset >= 0 ? "+" : "-";
+  const pad = (n: number) => `${Math.floor(Math.abs(n))}`.padStart(2, "0");
+  return (
+    date.getFullYear() +
+    "-" +
+    pad(dateObj.getMonth() + 1) +
+    "-" +
+    pad(dateObj.getDate()) +
+    "T" +
+    pad(dateObj.getHours()) +
+    ":" +
+    pad(dateObj.getMinutes()) +
+    ":" +
+    pad(dateObj.getSeconds()) +
+    diff +
+    pad(tzOffset / 60) +
+    ":" +
+    pad(tzOffset % 60)
+  );
+};
 
-const time = `${date.getFullYear()}년 ${
-  date.getMonth() + 1
-}월 ${date.getDay()}일 ${date.getHours()}시 ${date.getMinutes()}분 ${date.getSeconds()}초`;
+const time = `날짜  ${(date.getMonth() + 1).toString()}/${date.getDate()}
+시간  ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.${date.getMilliseconds()}
+빌드 타임  ??
+`;
 
 if (buildLog.includes("success")) {
   const buildTextAttach = new AttachmentBuilder(
@@ -37,11 +63,12 @@ if (buildLog.includes("success")) {
       username: "petBot",
       avatarURL:
         "https://cdn.discordapp.com/app-icons/1044621624864940163/87fe18353f90a7a4c275be945afc14e5.png?size=512",
+      // embeds: [embedContents(`${time} 빌드 성공! \n빌드타임 : ??`)],
       embeds: [
-        embedContents(`${time} 빌드 성공! \n빌드타임 : ??`).setColor(0x008d62),
+        embedContents(`petBook Web Client 빌드 성공 !`).setColor(0x008d62),
       ],
       files: [buildTextAttach],
-      content: `종료시간 : ${new Date().toISOString()}`,
+      content: `빌드 성공\n종료시간 : ${date.toString()}`,
     })
     .then((d) => d)
     .catch((e) => e);
@@ -56,8 +83,11 @@ if (buildLog.includes("success")) {
       username: "petBot",
       avatarURL:
         "https://cdn.discordapp.com/app-icons/1044621624864940163/87fe18353f90a7a4c275be945afc14e5.png?size=512",
-      embeds: [embedContents(`${time} 빌드 실패`).setColor(0x9b111e)],
+      embeds: [
+        embedContents(`petBook Web Client 빌드 실패`).setColor(0x9b111e),
+      ],
       files: [errorTextAttach],
+      content: `빌드 실패\n종료시간 : ${date.toString()}`,
     })
     .then((d) => d)
     .catch((e) => e);
--- petbook_fe/scripts/preRunner.ts
+++ petbook_fe/scripts/preRunner.ts
@@ -1,33 +1,61 @@
 import { EmbedBuilder, WebhookClient } from "discord.js";
 import * as dotenv from "dotenv";
-// import { loadEnvConfig } from "@next/env";
-// import fs from "fs";
-// import path from "path";
-// import localConsole from "@lib/utils/localConsole";
+import envSelector from "./envSelector";
 
 dotenv.config();
 
+const env = envSelector("develop");
+
 const webhookClient = new WebhookClient({
-  url: process.env.TEST_WEBHOOK_URL as string,
+  url: env.webhookURL,
 });
 
 const embedContents = (content: string) => {
-  return new EmbedBuilder().setTitle(content).setColor(0xffd400);
+  return new EmbedBuilder()
+    .setTitle(content)
+    .setColor(0xffd400)
+    .setDescription(`${time}`);
 };
 
 const date = new Date();
 
-const time = `${date.getFullYear()}년 ${
-  date.getMonth() + 1
-}월 ${date.getDay()}일 ${date.getHours()}시 ${date.getMinutes()}분 ${date.getSeconds()}초`;
+const toISOStringWithTimezone = (dateObj: Date) => {
+  const tzOffset = -date.getTimezoneOffset();
+  const diff = tzOffset >= 0 ? "+" : "-";
+  const pad = (n: number) => `${Math.floor(Math.abs(n))}`.padStart(2, "0");
+  return (
+    date.getFullYear() +
+    "-" +
+    pad(dateObj.getMonth() + 1) +
+    "-" +
+    pad(dateObj.getDate()) +
+    "T" +
+    pad(dateObj.getHours()) +
+    ":" +
+    pad(dateObj.getMinutes()) +
+    ":" +
+    pad(dateObj.getSeconds()) +
+    diff +
+    pad(tzOffset / 60) +
+    ":" +
+    pad(tzOffset % 60)
+  );
+};
+
+// const latest =
+
+const time = `날짜  ${(date.getMonth() + 1).toString()}/${date.getDate()}
+시간  ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.${date.getMilliseconds()}
+마지막 업데이트  ??
+`;
 
 webhookClient
   .send({
     username: "petBot",
     avatarURL:
       "https://cdn.discordapp.com/app-icons/1044621624864940163/87fe18353f90a7a4c275be945afc14e5.png?size=512",
-    embeds: [embedContents(`${time} 부터 빌드 시작`)],
-    content: `빌드중... \n시작시간 : ${new Date().toISOString()}
+    embeds: [embedContents(`petBook Web Client 빌드 시작`)],
+    content: `빌드중... \n시작시간 : ${date.toString()}
     `,
   })
   .then((d) => d)
