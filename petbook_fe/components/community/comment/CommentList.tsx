52d617f54547cad496addf16b66f64672699f222
Eunbi <jenabill@naver.com>
2022-12-30 22:39:31 +0900

    refactor: CommentList component

--- petbook_fe/components/community/comment/CommentList.tsx
+++ petbook_fe/components/community/comment/CommentList.tsx
@@ -1,4 +1,3 @@
-import { BsArrowReturnRight } from "react-icons/bs";
 import useResource, { useSetResource } from "@lib/hooks/common/useResource";
 import {
   COMMENT_CREATE_LIKE,
@@ -8,32 +7,26 @@ import {
 } from "@pages/community/[articleId]";
 import { useRouter } from "next/router";
 import { CommentItem } from "@lib/API/petBookAPI/types/commentRequest";
-import React, { Fragment, MouseEventHandler, useRef, useState } from "react";
+import React, { Fragment, MouseEventHandler, useRef } from "react";
 import { useQueryClient } from "react-query";
-import styled from "styled-components";
-import DropdownMenu, { menuListStyle } from "@components/common/DropdownMenu";
 import debounce from "@lib/modules/debounce";
-import CommonInfo from "../CommonInfo";
-import {
-  CommentListDiv,
-  NormalItemDiv,
-  LikeButton,
-  ScrapButton,
-  QnaItemDiv,
-  QnaItemBubble,
-} from "./styled/styledCommentList";
+import { CommentListDiv } from "./styled/styledCommentList";
 
-const avatar =
-  "https://images.unsplash.com/photo-1518796745738-41048802f99a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFiYml0fGVufDB8fDB8fA%3D%3D&w=1000&q=80";
-
-interface Props {
-  Item: (props: ItemProps) => JSX.Element;
-}
-
-interface onLikeParams {
+export interface onLikeParams {
   commentId: number;
   isLiked: boolean;
   initialLiked: boolean;
+}
+
+export interface ItemProps {
+  comment: CommentItem;
+  isChild: string;
+  onDelete: MouseEventHandler<HTMLButtonElement>;
+  onLike: (params: onLikeParams) => void;
+}
+
+interface Props {
+  Item: (props: ItemProps) => JSX.Element;
 }
 
 const CommentList = ({ Item }: Props) => {
@@ -130,153 +123,5 @@ const CommentList = ({ Item }: Props) => {
     </CommentListDiv>
   );
 };
-
-// ----------------------------------------------------
-
-interface ItemProps {
-  comment: CommentItem;
-  isChild: string;
-  onDelete: MouseEventHandler<HTMLButtonElement>;
-  onLike: (params: onLikeParams) => void;
-}
-
-export const NormalItem = ({
-  comment,
-  isChild,
-  onDelete,
-  onLike,
-}: ItemProps) => {
-  const {
-    user,
-    createdAt,
-    content,
-    likeCount,
-    id,
-    articleId,
-    isLiked: initialLiked,
-  } = comment;
-  const [isLiked, setIsLiked] = useState(initialLiked);
-  const clickLikeButton = () => {
-    onLike({ commentId: id, isLiked: !isLiked, initialLiked });
-    setIsLiked((state) => !state);
-  };
-  return (
-    <NormalItemDiv isChild={isChild}>
-      {isChild && <BsArrowReturnRight />}
-      <div>
-        <div className="Item_Row">
-          <CommonInfo
-            username={user.nickname}
-            date={createdAt}
-            avatar={avatar}
-            year={1}
-          />
-          <DropdownMenu
-            MenuList={<MenuList onDelete={onDelete} commentId={id} />}
-          />
-        </div>
-        <p className="Item_Content">{content}</p>
-        <div className="Item_Button_Box">
-          <div>
-            <LikeButton
-              type="button"
-              onClick={clickLikeButton}
-              isLiked={isLiked ? "true" : ""}
-            />
-            <span className="Item_likeCount">
-              {likeCount +
-                (!initialLiked && isLiked
-                  ? 1
-                  : initialLiked && !isLiked
-                  ? -1
-                  : 0)}
-            </span>
-          </div>
-          <div>
-            <ScrapButton type="button" />
-            <span>0</span>
-          </div>
-        </div>
-      </div>
-    </NormalItemDiv>
-  );
-};
-
-export const QnaItem = ({ comment, isChild, onDelete, onLike }: ItemProps) => {
-  const {
-    user,
-    createdAt,
-    content,
-    likeCount,
-    id,
-    articleId,
-    isLiked: initialLiked,
-  } = comment;
-  const [isLiked, setIsLiked] = useState(initialLiked);
-  const clickLikeButton = () => {
-    onLike({ commentId: id, isLiked: !isLiked, initialLiked });
-    setIsLiked((state) => !state);
-  };
-  return (
-    <QnaItemDiv>
-      <CommonInfo
-        username={user.nickname}
-        date={createdAt}
-        avatar={avatar}
-        year={1}
-      />
-      <QnaItemBubble>
-        <div className="Item_Row">
-          <p className="Item_Content">{content}</p>
-          <DropdownMenu
-            MenuList={<MenuList onDelete={onDelete} commentId={comment.id} />}
-          />
-        </div>
-        <div className="Item_Button_Box">
-          <div>
-            <LikeButton
-              type="button"
-              onClick={clickLikeButton}
-              isLiked={isLiked ? "true" : ""}
-            />
-            <span className="Item_likeCount">
-              {likeCount +
-                (isLiked && !initialLiked
-                  ? 1
-                  : !isLiked && initialLiked
-                  ? -1
-                  : 0)}
-            </span>
-          </div>
-          <div>
-            <ScrapButton type="button" />
-            <span>0</span>
-          </div>
-        </div>
-      </QnaItemBubble>
-    </QnaItemDiv>
-  );
-};
-
-// -------------------------------------------------
-
-interface MenuListProps extends Pick<ItemProps, "onDelete"> {
-  commentId: number;
-}
-
-const MenuList = ({ onDelete, commentId }: MenuListProps) => {
-  return (
-    <MenuListBox>
-      <button type="button">수정</button>
-      <button type="button" onClick={onDelete} data-commentid={commentId}>
-        삭제
-      </button>
-    </MenuListBox>
-  );
-};
-
-const MenuListBox = styled.div`
-  ${menuListStyle};
-`;
 
 export default CommentList;
