5d82dad196a5aefc15c0fd5b47226398b5d0f24d
Eunbi <jenabill@naver.com>
2022-12-30 22:38:13 +0900

    refactor: WriteButton component

--- petbook_fe/components/community/WriteButton.tsx
+++ petbook_fe/components/community/WriteButton.tsx
@@ -16,18 +16,19 @@ const StyledWriteButton = styled.button`
   color: #fff;
   font-size: 18px;
   box-shadow: 0px 24px 16px -16px rgba(69, 66, 54, 0.25);
-  span {
-    width: 20px;
-    height: 20px;
-    background: url("/img/common/pencil_edit.svg") no-repeat;
-  }
+`;
+
+const PencilIcon = styled.span`
+  width: 20px;
+  height: 20px;
+  background: url("/img/common/pencil_edit.svg") no-repeat;
 `;
 
 const WriteButton = () => {
   return (
     <Link href="/community/write" passHref>
       <StyledWriteButton>
-        <span />
+        <PencilIcon />
         글쓰기
       </StyledWriteButton>
     </Link>
