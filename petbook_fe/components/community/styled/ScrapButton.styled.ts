5469bcacca3d5d689bed5b1255fb6a94a840917b
Eunbi <jenabill@naver.com>
2022-12-30 22:38:41 +0900

    add common styled components

---
+++ petbook_fe/components/community/styled/ScrapButton.styled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const ScrapButton = styled.button`
+  width: 24px;
+  height: 24px;
+  background: url("/img/common/scrap.svg") no-repeat center center;
+`;
+
+export default ScrapButton;
