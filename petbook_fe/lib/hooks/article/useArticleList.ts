1fc85078b676e8dd91512227e641b0ba62fb1f70
Eunbi <jenabill@naver.com>
2023-01-01 01:31:19 +0900

    update useArticleList hook

--- petbook_fe/lib/hooks/article/useArticleList.ts
+++ petbook_fe/lib/hooks/article/useArticleList.ts
@@ -1,11 +1,10 @@
-import categoryState from "@atoms/pageAtoms/community/categoryState";
-import { ARTICLE_LIST } from "@pages/community";
+import { ARTICLE_LIST } from "@pages/community/list";
 import { useRef } from "react";
-import { useRecoilValue } from "recoil";
 import { ArticleListResponse } from "@lib/API/petBookAPI/types/articleRequest";
 import { UseQueryResult } from "react-query";
 import { usePage } from "./usePagination";
 import useResource from "../common/useResource";
+import useCategory from "./useCategory";
 
 export default function useArticleList(): {
   status: UseQueryResult["status"];
@@ -14,18 +13,18 @@ export default function useArticleList(): {
 } {
   // params
   const size = useRef(10);
-  const category = useRecoilValue(categoryState);
   const page = usePage();
+  const { categoryName, categoryId } = useCategory();
 
   // fetch data
   const { data, status } = useResource({
-    key: `${ARTICLE_LIST.key}_${page}_${category.id}`,
+    key: `${ARTICLE_LIST.key}_${categoryName}_${page}`,
     fetcher: ARTICLE_LIST.fetcher,
     params: {
-      categoryId: category.id === 0 ? "" : category.id,
+      categoryId: categoryId === 0 ? "" : categoryId,
       page: page - 1,
       size: size.current,
-    },
+    }
   });
 
   // return data
