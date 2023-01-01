af196756b1ca992dd49c95cc758faeed058fc122
Eunbi <jenabill@naver.com>
2023-01-01 19:28:52 +0900

    refactor: CommentList component

--- petbook_fe/components/community/comment/CommentList.tsx
+++ petbook_fe/components/community/comment/CommentList.tsx
@@ -1,23 +1,18 @@
 import useResource, { useSetResource } from "@lib/hooks/common/useResource";
 import {
-  COMMENT_CREATE_LIKE,
   COMMENT_DELETE,
-  COMMENT_DELETE_LIKE,
   COMMENT_LIST,
 } from "@pages/community/list/[articleId]";
 import { useRouter } from "next/router";
 import { CommentItem } from "@lib/API/petBookAPI/types/commentRequest";
-import React, { Fragment, MouseEventHandler, useRef } from "react";
+import React, { Fragment, MouseEventHandler } from "react";
 import { useQueryClient } from "react-query";
-import debounce from "@lib/modules/debounce";
-import { updateIsLikedParams } from "@lib/hooks/comment/useLike";
 import { CommentListDiv } from "./styled/styledCommentList";
 
 export interface ItemProps {
   comment: CommentItem;
   isChild: string;
   onDelete: MouseEventHandler<HTMLButtonElement>;
-  updateIsLiked: (params: updateIsLikedParams) => void;
 }
 
 interface Props {
@@ -36,9 +31,6 @@ const CommentList = ({ Item }: Props) => {
       }),
   });
   const { mutate: deleteComment } = useSetResource(COMMENT_DELETE);
-  const { mutate: createLikeComment } = useSetResource(COMMENT_CREATE_LIKE);
-  const { mutate: deleteLikeComment } = useSetResource(COMMENT_DELETE_LIKE);
-  const timeoutId = useRef<ReturnType<typeof setTimeout> | null>(null);
   const onSuccess = async () => {
     await queryClient.invalidateQueries(`${COMMENT_LIST.key}_${articleId}`);
   };
@@ -61,38 +53,6 @@ const CommentList = ({ Item }: Props) => {
     }
   };
 
-  const updateIsLiked = debounce(
-    ({ commentId, isLiked, initialLiked }: updateIsLikedParams) => {
-      if (isLiked === initialLiked) return;
-      if (!isLiked) {
-        deleteLikeComment(
-          {
-            pathParam: `${commentId}`,
-          },
-          {
-            onSuccess,
-            onError: () => {
-              alert("좋아요 취소에 실패했습니다. 다시 시도해주세요 😢");
-            },
-          }
-        );
-      } else {
-        createLikeComment(
-          {
-            pathParam: `${commentId}`,
-          },
-          {
-            onSuccess,
-            onError: () => {
-              alert("좋아요에 실패했습니다. 다시 시도해주세요 😢");
-            },
-          }
-        );
-      }
-    },
-    3000,
-    timeoutId
-  );
   return (
     <CommentListDiv>
       {data?.data.map((comment) => (
@@ -101,7 +61,6 @@ const CommentList = ({ Item }: Props) => {
             comment={comment.parent}
             isChild=""
             onDelete={onDelete}
-            updateIsLiked={updateIsLiked}
             key={comment.parent.id}
           />
           {comment.children.map((child) => (
@@ -109,7 +68,6 @@ const CommentList = ({ Item }: Props) => {
               comment={child}
               isChild="true"
               onDelete={onDelete}
-              updateIsLiked={updateIsLiked}
               key={child.id}
             />
           ))}
