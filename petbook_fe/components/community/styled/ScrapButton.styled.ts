7e3607b8ed5baac41bc93856c6e1e3bbd0e1c9be
Eunbi <jenabill@naver.com>
2023-01-01 14:23:43 +0900

    delete: LikeButton, ScrapButton

--- petbook_fe/components/community/styled/ScrapButton.styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const ScrapButton = styled.button`
-  width: 24px;
-  height: 24px;
-  background: url("/img/common/scrap.svg") no-repeat center center;
-`;
-
-export default ScrapButton;
