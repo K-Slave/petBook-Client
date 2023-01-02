acc3c4330cfee0af0f155f95ea74b5dd11fe1b61
Eunbi <jenabill@naver.com>
2023-01-03 00:11:30 +0900

    rename: atoms/common folder

--- petbook_fe/components/common/Modal.tsx
+++ petbook_fe/components/common/Modal.tsx
@@ -1,11 +1,9 @@
-import modalState from "@atoms/pageAtoms/community/modalState";
+import modalState from "@atoms/common/modalState";
 import { useRecoilValue } from "recoil";
 
 const Modal = () => {
-    const { Component, props } = useRecoilValue(modalState);
-    return (
-        Component ? <Component {...props} /> : null
-    );
+  const { Component, props } = useRecoilValue(modalState);
+  return Component ? <Component {...props} /> : null;
 };
 
 export default Modal;
