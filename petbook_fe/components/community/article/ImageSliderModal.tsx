5e95b56a7f4ee47de54829b8604168b372f80c02
Eunbi <jenabill@naver.com>
2023-01-02 23:03:33 +0900

    refactor: ImageSliderModal component

--- petbook_fe/components/community/article/ImageSliderModal.tsx
+++ petbook_fe/components/community/article/ImageSliderModal.tsx
@@ -2,21 +2,26 @@ import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { useRef } from "react";
 import imageModalState from "@atoms/pageAtoms/community/imageModalState";
-import CustomSwiper, { SlideNextButton, SlidePrevButton } from "@components/common/CustomSwiper";
+import CustomSwiper, {
+  SlideNextButton,
+  SlidePrevButton,
+} from "@components/common/CustomSwiper";
 import { SwiperSlide } from "swiper/react";
 import Image from "next/image";
 import useClickOutside from "@lib/hooks/common/useClickOutside";
+import { Container } from "../CommunityModal/styled";
 
 const prevElId = "swiper_back";
 const nextElId = "swiper_forward";
 
 const ImageSliderModal = () => {
   const ref = useRef<HTMLDivElement | null>(null);
-  const [{ show, images, initialImageIndex }, setImageModalState] = useRecoilState(imageModalState);
-  const closeModal = () => setImageModalState((state) => ({ ...state, show: false }));
+  const [{ show, images, initialImageIndex }, setImageModalState] =
+    useRecoilState(imageModalState);
+  const closeModal = () =>
+    setImageModalState((state) => ({ ...state, show: false }));
   useClickOutside(ref, closeModal);
-  return (
-    show ?
+  return show ? (
     <Container>
       <SliderDiv ref={ref}>
         <SlidePrevButton prevElId={prevElId} />
@@ -26,32 +31,17 @@ const ImageSliderModal = () => {
           prevElId={prevElId}
           nextElId={nextElId}
         >
-          {images.map((image) =>
+          {images.map((image) => (
             <SwiperSlide key={image.id}>
               <Image src={image.imageUrl} layout="fill" objectFit="cover" />
             </SwiperSlide>
-          )}
+          ))}
         </CustomSwiper>
         <SlideNextButton nextElId={nextElId} />
       </SliderDiv>
-    </Container> :
-    null
-  );
+    </Container>
+  ) : null;
 };
-
-const Container = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  z-index: 200;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: rgba(86, 86, 83, 0.4);
-  backdrop-filter: blur(15px);
-`;
 
 const SliderDiv = styled.div`
   display: flex;
