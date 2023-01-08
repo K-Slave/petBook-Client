eaa7adc50d6852bda936b50e11f4b7791ed4c491
steven-yn <steven_yoon1009@naver.com>
2023-01-08 23:36:10 +0900

    FIX: 이미지 파일 획득후 새 파일 생성하여 파일명 replace 처리

--- petbook_fe/lib/utils/replaceAll.ts
+++ petbook_fe/lib/utils/replaceAll.ts
@@ -1,15 +1,17 @@
-export default function replaceAll(
-  input: string,
-  target: string,
-  noBlank?: boolean
-): string | undefined {
-  if (!input.includes(target)) return undefined;
-  let temp = [];
+// string.prototype.replaceAll 이 safari < 13.1, node < 15 에서 동작하지 않으므로
 
-  const splitedInput = input.split(target);
-  temp =
-    noBlank === true ? splitedInput.map((str) => str.trim()) : splitedInput;
-  const joinedInput = temp.join("");
+// TODO : 대소문자 구분 잘못됨 ( 소문자 검색 -> 대문자 허용 만 되고 반대는 안됌)
+// 해당 함수로 대체하여 사용
+const replaceAll = (
+  target: string,
+  replaceTarget: string,
+  replaceText: string,
+  originStrList?: string[]
+) => {
+  const splitedInput = target.split(replaceTarget);
+
+  const joinedInput = splitedInput.join(replaceText);
 
   return joinedInput;
-}
+};
+export default replaceAll;
