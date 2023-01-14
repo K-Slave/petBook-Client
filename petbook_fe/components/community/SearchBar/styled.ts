2c7e2a5f7911c3a1d06bbc7cb7c824ab6282a165
Eunbi <jenabill@naver.com>
2023-01-15 00:29:56 +0900

    add SearchBar component

---
+++ petbook_fe/components/community/SearchBar/styled.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Bar = styled.div`
+  display: flex;
+  align-items: center;
+  padding-left: 16px;
+  padding-right: 8px;
+  gap: 1rem;
+  background: #ffffff;
+  border: 1px solid var(--black_04);
+  box-shadow: 0px 20px 16px -20px #d6d3c5;
+  border-radius: 8px;
+`;
+
+export const Input = styled.input`
+  height: 40px !important;
+  padding: 0 !important;
+  background-color: transparent !important;
+  outline: none !important;
+`;
