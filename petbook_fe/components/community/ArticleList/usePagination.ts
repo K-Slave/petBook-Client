afbbdcb584774a382824268ce402356e175612fb
Eunbi <jenabill@naver.com>
2023-01-17 13:13:44 +0900

    delete useLoaderNavigate hook

--- petbook_fe/components/community/ArticleList/usePagination.ts
+++ petbook_fe/components/community/ArticleList/usePagination.ts
@@ -1,7 +1,6 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import localConsole from "@lib/utils/localConsole";
-import useLoaderNavigate from "@lib/hooks/common/useLoaderNavigate";
+import navigator from "@lib/modules/navigator";
 
 export const usePage = () => {
   const router = useRouter();
@@ -22,7 +21,6 @@ export default function usePagination({
   const [offset, setOffset] = useState(1);
   const currentPage = usePage();
   const router = useRouter();
-  const { navigator } = useLoaderNavigate();
   const changeCurrentPage = (page: number) => {
     const params = new URLSearchParams(router.asPath.split("?")[1]);
     params.delete("page");
@@ -42,7 +40,6 @@ export default function usePagination({
   // currentPage가 1 ~ totalPages range에 존재하는지 판단
   useEffect(() => {
     if (totalPages === 0) return;
-    localConsole?.log("test page range");
     if (currentPage < 1) {
       changeCurrentPage(1);
     } else if (currentPage > totalPages) {
@@ -52,7 +49,6 @@ export default function usePagination({
 
   // currentPage 변화에 따른 button offset 판단
   useEffect(() => {
-    localConsole?.log("test button offset");
     if (currentPage >= offset + btnNum) {
       setOffset((oldOffset) => oldOffset + btnNum);
     } else if (currentPage < offset) {
