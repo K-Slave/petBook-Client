fa4258ca9455bba4d9cdff6a5a02021615a67e82
steven-yn <steven_yoon1009@naver.com>
2023-01-30 00:14:20 +0900

    fix: 페이지 이름 변경 및 공용 인터섹션 옵저버 추가

---
+++ petbook_fe/lib/hooks/common/useIntersectionObserver.ts
@@ -0,0 +1,150 @@
+import { useRef } from "react";
+import throttle from "lodash.throttle";
+import localConsole from "@lib/utils/localConsole";
+
+export default function useIntersectionObserver({
+  onAppearCallback,
+  onDisappearCallback,
+  $RootDom,
+  rootMargin,
+  isScrollObserver,
+}: {
+  onAppearCallback?: () => void;
+  onDisappearCallback?: () => void;
+  $RootDom?: HTMLDivElement | null;
+  rootMargin: string | number;
+  isScrollObserver?: boolean;
+}) {
+  const targetElement = useRef<HTMLDivElement>(null);
+  const appearObserver = useRef<IntersectionObserver>();
+  const disAppearObserver = useRef<IntersectionObserver>();
+  const isSet = useRef<boolean>(false);
+
+  const onAppear = (entries: IntersectionObserverEntry[]) => {
+    entries.forEach(async (entry) => {
+      if (entry.isIntersecting && onAppearCallback) {
+        onAppearCallback();
+      }
+    });
+  };
+
+  const onDisappear = (entries: IntersectionObserverEntry[]) => {
+    entries.forEach(async (entry: any) => {
+      if (!entry.isVisible && onDisappearCallback) {
+        onDisappearCallback();
+      }
+      // if (entry.isIntersecting) {
+      //   onAppearCallback();
+      // }
+      // if (entry.isVisible) {
+      //   onAppearCallback();
+      // }
+    });
+  };
+
+  const infiniteScroll = throttle(() => {
+    try {
+      const currentScroll = $RootDom?.scrollTop;
+      const scrollHeight = $RootDom?.scrollHeight;
+      const clientHeight = targetElement.current?.clientHeight;
+      const paddingBottom = typeof rootMargin === "number" ? rootMargin : 50;
+
+      if (currentScroll && scrollHeight && clientHeight && onAppearCallback) {
+        if (
+          currentScroll + window.innerHeight + paddingBottom >=
+          scrollHeight
+        ) {
+          onAppearCallback();
+        }
+      }
+    } catch (e) {
+      localConsole?.log(e);
+    }
+  }, 300);
+
+  const setObserver = () => {
+    if (!window.IntersectionObserver) {
+      if (isSet.current === false && isScrollObserver === true) {
+        isSet.current = true;
+
+        if (!$RootDom) {
+          document.body.addEventListener("scroll", infiniteScroll);
+
+          return;
+        }
+        $RootDom.addEventListener("scroll", infiniteScroll);
+      }
+
+      // TODO: 스크롤 이벤트 핸들링시 disappear 동작 구현해야함
+      // if (!isScrollObserver && onDisappearCallback) {
+      //   onDisappearCallback();
+      // }
+    } else {
+      try {
+        if (onAppearCallback && !appearObserver.current) {
+          appearObserver.current = new IntersectionObserver(onAppear, {
+            root: $RootDom || document.body,
+            rootMargin:
+              typeof rootMargin === "number"
+                ? rootMargin.toString() + "px"
+                : rootMargin,
+          });
+        }
+
+        if (onDisappearCallback && !disAppearObserver.current) {
+          disAppearObserver.current = new IntersectionObserver(onDisappear, {
+            root: $RootDom || document.body,
+            rootMargin:
+              typeof rootMargin === "number"
+                ? rootMargin.toString() + "px"
+                : rootMargin,
+          });
+        }
+
+        if (targetElement.current) {
+          appearObserver.current?.observe(targetElement.current);
+          disAppearObserver.current?.observe(targetElement.current);
+        }
+      } catch (e) {
+        localConsole?.log(e);
+      }
+    }
+  };
+
+  const unsetObserver = () => {
+    if (isScrollObserver === true) {
+      if (!$RootDom) return;
+
+      $RootDom.removeEventListener("scroll", infiniteScroll);
+    }
+
+    if (!window.IntersectionObserver) {
+      if (isScrollObserver === true) {
+        if (!$RootDom) {
+          document.body.removeEventListener("scroll", infiniteScroll);
+
+          return;
+        }
+
+        $RootDom.removeEventListener("scroll", infiniteScroll);
+      }
+    } else {
+      try {
+        // 12.1 >= safari
+
+        if (targetElement.current) {
+          appearObserver.current?.unobserve(targetElement.current);
+          disAppearObserver.current?.unobserve(targetElement.current);
+        }
+      } catch (e) {
+        localConsole?.log(e);
+      }
+    }
+  };
+
+  return [targetElement, setObserver, unsetObserver] as [
+    typeof targetElement,
+    typeof setObserver,
+    typeof unsetObserver
+  ];
+}
