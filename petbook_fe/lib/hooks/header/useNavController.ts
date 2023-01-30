4255851b9736239df2e450417a43f973d2dbd79b
steven-yn <steven_yoon1009@naver.com>
2023-01-31 02:07:01 +0900

    fix: 스크롤 핸들러 성능 최적화

--- petbook_fe/lib/hooks/header/useNavController.ts
+++ petbook_fe/lib/hooks/header/useNavController.ts
@@ -1,19 +1,41 @@
+import localConsole from "@lib/utils/localConsole";
 import throttle from "lodash.throttle";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 const useNavController = () => {
   const [isNeedNav, setIsNeedNav] = useState(false);
+  const load = useRef(false);
 
   const scrollHandler = useCallback(
     throttle(() => {
+      if (load.current === true) return;
+      if (window.scrollY > 58 && isNeedNav === true) return;
+      if (window.scrollY < 58 && isNeedNav === false) return;
+
+      localConsole?.log("asdasdas");
+
       if (window.scrollY > 58 && isNeedNav === false) {
+        load.current = true;
         setIsNeedNav(true);
+
+        setTimeout(() => {
+          if (load.current === true) {
+            load.current = false;
+          }
+        }, 50);
 
         return;
       }
 
       if (window.scrollY < 58 && isNeedNav === true) {
+        load.current = true;
         setIsNeedNav(false);
+
+        setTimeout(() => {
+          if (load.current === true) {
+            load.current = false;
+          }
+        }, 50);
 
         return;
       }
@@ -23,7 +45,9 @@ const useNavController = () => {
 
   useEffect(() => {
     window.addEventListener("scroll", scrollHandler);
+
     return () => {
+      load.current = false;
       window.removeEventListener("scroll", scrollHandler);
     };
   }, [isNeedNav]);
