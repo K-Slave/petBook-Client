03a1bb0d11f85fd3f5749ab89688f2a331320796
Eunbi <jenabill@naver.com>
2023-02-01 22:00:46 +0900

    refactor: HospitalItem component

--- petbook_fe/components/hospital/HospitalItem/styled.ts
+++ petbook_fe/components/hospital/HospitalItem/styled.ts
@@ -1,87 +1,21 @@
 import { SliderButton } from "@components/common/Slider/styled";
 import styled from "styled-components";
 
-export const ItemArticle = styled.article`
-  .Item_Heading {
+export const ItemHeader = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0.75rem 0;
+  color: var(--black_01);
+  & > h1:hover {
+    cursor: pointer;
+    text-decoration: underline;
+  }
+  & > button {
     display: flex;
+    justify-content: flex-end;
     align-items: center;
-    justify-content: space-between;
-    margin: 0.75rem 0.5rem;
-    color: var(--black_01);
-  }
-  .Item_Stats {
-    display: flex;
-    gap: 0.75rem;
-    margin-bottom: 0.75rem;
-    font-weight: 500;
-    color: var(--black_04);
-  }
-  .Item_Info_List {
-    display: flex;
-    flex-direction: column;
-    gap: 0.75rem;
-    padding: 0 0.5rem;
-    margin: 0.75rem 0 1.5rem;
-    li {
-      display: flex;
-      align-items: center;
-      gap: 0.5rem;
-      color: var(--black_03);
-      font-weight: 500;
-      font-size: 16px;
-      & > span:first-child {
-        display: flex;
-        align-items: center;
-        gap: 0.375rem;
-        color: var(--black_06);
-      }
-    }
-  }
-`;
-
-export const AnimalListBox = styled.div`
-  background-color: #ffefd1;
-  border-radius: 12px;
-  padding: 0.75rem;
-  & > div {
-    display: flex;
-    align-items: center;
-    gap: 0.25rem;
-    margin-bottom: 0.75rem;
-    & > h2 {
-      display: flex;
-      align-items: flex-end;
-      gap: 0.3rem;
-      color: var(--black_01);
-      font-size: 14px;
-      font-weight: bold;
-    }
-  }
-  & > ul {
-    display: flex;
-    align-items: center;
-    flex-wrap: wrap;
-    gap: 0.5rem;
-    & > li {
-      display: flex;
-      align-items: center;
-      gap: 0.25rem;
-      color: var(--black_04);
-      font-size: 14px;
-    }
-    .icon {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      width: 32px;
-      height: 32px;
-      border-radius: 50%;
-      background-color: white;
-    }
-    img {
-      width: auto;
-      object-fit: cover;
-    }
+    color: inherit;
   }
 `;
 
@@ -108,19 +42,4 @@ export const ImageSliderDiv = styled.div`
     width: 100%;
     height: 100%;
   }
-`;
-
-export const Badge = styled.div<{
-  textColor: string;
-  bgColor: string;
-}>`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 0.25rem 0.5rem;
-  color: ${({ textColor }) => textColor};
-  background: ${({ bgColor }) => bgColor};
-  border-radius: 20px;
-  font-size: 12px;
-  font-weight: bold;
 `;
