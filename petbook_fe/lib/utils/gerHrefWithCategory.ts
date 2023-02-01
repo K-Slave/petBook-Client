9abd3f784e669adb2ac223ad62cf5c3afeed067d
Eunbi <jenabill@naver.com>
2023-02-01 17:53:42 +0900

    refactor: getHrefWithCategory function

--- petbook_fe/lib/utils/gerHrefWithCategory.ts
+++ petbook_fe/lib/utils/gerHrefWithCategory.ts
@@ -1,7 +1,5 @@
 import { CategoryItem } from "@lib/API/petBookAPI/types/categoryRequestSpr";
 
 export default function getHrefWithCategory(category: CategoryItem) {
-  return `/community/list?category=${category.name ? category.name : ""}_${
-    category.id ? category.id : ""
-  }`;
+  return `/community/list?category=${category.name}_${category.id}`;
 }
