90e739e9d7fbc6a4dbeb5754582351e7276dc5df
steven-yn <steven_yoon1009@naver.com>
2023-02-08 01:13:32 +0900

    fix: 필요없는 코드 정리

--- petbook_fe/components/map/KakaoMap.style.tsx
+++ petbook_fe/components/map/KakaoMap.style.tsx
@@ -1,4 +1,4 @@
-import { Map, MapInfoWindow, MapMarker } from "react-kakao-maps-sdk";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 import styled from "styled-components";
 
 export const KakaoMapDiv = styled(Map)`
@@ -7,11 +7,3 @@ export const KakaoMapDiv = styled(Map)`
 `;
 
 export const KakaoMapMarker = styled(MapMarker)``;
-
-export const MarkerInfoWindowStyle: React.CSSProperties = {};
-
-export const MarkerInfoWindowDiv = styled(MapInfoWindow)`
-  .text {
-    color: #ccc;
-  }
-`;
