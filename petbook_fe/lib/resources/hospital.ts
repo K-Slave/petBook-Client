da543771efa76df54eda9796d672c38ee0b43393
Eunbi <jenabill@naver.com>
2023-03-21 14:09:24 +0900

    refactor: 주석 수정

--- petbook_fe/lib/resources/hospital.ts
+++ petbook_fe/lib/resources/hospital.ts
@@ -13,7 +13,7 @@ const hospitalKeys = {
   ): Key => ["HOSPITAL_DETAIL", args[0].pathParam],
 };
 
-// resource들을 한 곳에서 관리하는 방법 테스트, redux 처럼 사용
+// resource들을 한 곳에서 관리하는 방법 테스트
 /*
 WHY??
 - 페이지마다 중복되서 사용하는 resource 존재 => 중복 제거
@@ -21,7 +21,7 @@ WHY??
 - queryParser 함수에서 각 resource에 대해 엄격한 타입 사용 가능
 */
 export const HOSPITAL_LIST = {
-  // name: resource 이름 (redux로 보면 액선 이름, queryParser 함수에서 분기점으로 사용)
+  // name: resource 이름 (queryParser 함수에서 분기점으로 사용)
   name: "HOSPITAL_LIST" as const,
   // createQuery: resource의 query 키와 fetcher 함수를 지닌 객체를 반환하는 함수, 이 함수의 파라미터로 payload를 전달하면 됨
   createQuery: getCreateQueryFn({
