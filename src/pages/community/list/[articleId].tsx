594a760d088bc044201ebbe5615e62d4570149fe
parksol <thfdl0317@naver.com>
2023-08-23 16:07:39 +0900

    add : 이런 글 어때요 컴포넌트 추가

--- src/pages/community/list/[articleId].tsx
+++ src/pages/community/list/[articleId].tsx
@@ -1,29 +1,38 @@
 import styled from "styled-components";
 import BackButton from "@components/community/BackButton";
+import RecommendWriteSection from "@components/community/RecommendWriteSection";
 import ArticleContainer from "@containers/article/ArticleContainer";
 import { NextPageWithOptions } from "@lib/queries";
 import { ARTICLE_DETAIL } from "@lib/resources/articleResource";
 import { COMMENT_LIST } from "@lib/resources/commentResource";
 import commonServerSideProps from "@lib/server/commonServerSideProps";
+import RecommendWriteList from "@components/community/RecommendWriteList";
 
 const ArticleDetail: NextPageWithOptions = () => {
   return (
-    <Main>
-      <BackButton position="start" />
-      <ArticleContainer />
-      <BackButton position="end" />
-    </Main>
+    <>
+      <Main>
+        <BackButton position="start" />
+        <ArticleContainer />
+        <BackButton position="end" />
+      </Main>
+      <RecommendWriteSection title="이런 글은 어때요?">
+        <RecommendWriteList />
+      </RecommendWriteSection>
+    </>
   );
 };
 
 const Main = styled.main`
+  width: 100%;
   display: flex;
   flex-direction: column;
-  gap: 20px;
-  max-width: 1330px;
-  width: 100%;
+  gap: 1.25rem;
+
   margin: 0 auto;
   padding: 52px 35px;
+  padding: 3.25rem 2.1875rem;
+  max-width: 83.125rem;
 `;
 
 export const getServerSideProps = commonServerSideProps([
