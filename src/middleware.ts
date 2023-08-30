0fb17ec6fa7691d0ed9a585ae226fcfcd97cd392
Yoon Sung Yeon <87164509+steven-yn@users.noreply.github.com>
2023-08-30 18:52:38 +0900

    hotfix: 미들웨어 에러 수정 1 (#169)

--- src/middleware.ts
+++ src/middleware.ts
@@ -70,3 +70,16 @@ export function middleware(request: NextRequest, event: NextFetchEvent) {
     return NextResponse.next();
   }
 }
+
+export const config = {
+  matcher: [
+    /*
+     * api (API 라우트)
+     * _next/static (정적 파일)
+     * _next/image (이미지 최적화 파일)
+     * favicon.ico (파비콘 파일)
+     * 로 시작하지 않는 모든 요청 경로와 일치합니다.
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
